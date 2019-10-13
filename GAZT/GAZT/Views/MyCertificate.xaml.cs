8aa7eb2cc75a6faae4f8fe79987381b9ae4d37c2
akashsaxena <akash.saxena@parallelminds.in>
2019-10-13 13:38:04 +0300

    Dashboard Bottom design implemented

---
+++ GAZT/GAZT/Views/MyCertificate.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace GAZT.Views
+{
+    public partial class MyCertificate : ContentPage
+    {
+        public MyCertificate()
+        {
+            InitializeComponent();
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPoint center = new SKPoint(info.Width / 2, info.Height / 2);
+            float radius = Math.Min(info.Width, info.Height) / 4;
+
+            SKPath path = new SKPath
+            {
+                FillType = SKPathFillType.EvenOdd,
+            };
+
+            float a = center.X - radius / 2;
+            float b = center.Y - radius / 2;
+            float r = radius;
+            float DeviceWidth = info.Width;
+            float deviceHeight = info.Height;
+            float XPoint = DeviceWidth / 2;
+            float YPoint = (deviceHeight * 40 / 100);// deviceHeight - ;
+            float Radius = deviceHeight + YPoint;
+            path.AddCircle(XPoint, -YPoint, Radius);
+
+
+            SKPaint paint = new SKPaint()
+            {
+                Style = SKPaintStyle.StrokeAndFill,
+                Color = SKColor.Parse("#005e4b"),
+            };
+
+            canvas.DrawPath(path, paint);
+
+        }
+
+
+    }
+}
