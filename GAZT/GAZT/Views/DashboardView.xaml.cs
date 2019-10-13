245243563073767adc4ec928a5e15af40c324a68
akashsaxena <akash.saxena@parallelminds.in>
2019-10-13 11:57:45 +0300

    Dashboard View Updated

--- GAZT/GAZT/Views/DashboardView.xaml.cs
+++ GAZT/GAZT/Views/DashboardView.xaml.cs
@@ -6,11 +6,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
-
+using GAZT.Helper;
 namespace GAZT.Views
 {
     public partial class DashboardView : ContentPage
     {
+        double DeviceHeight;
+        double DeviceWidth;
         ObservableCollection<String> Items = new ObservableCollection<String>();
         public DashboardView()
         {
@@ -18,7 +20,42 @@ namespace GAZT.Views
             string str = "abc";
             Items.Add(str);
             CardView.ItemsSource = Items;
+            EstimateZakatReturns.ItemsSource = Items;
+            MyCertificates.ItemsSource = Items;
+            MyBillsInvoices.ItemsSource = Items;
+            //DeviceWidth = DependencyService.Get<IDeviceInfo>().GetDeviceWidth();
+            //DeviceHeight = DependencyService.Get<IDeviceInfo>().GetDeviceHeight();
+            //SetEstimateZakatReturnsLayoutHeightWidth();
+            //SetMyCertificatesLayoutHeightWidth();
+            //SetMyBillsInvoicesLayoutHeightWidth();
         }
+
+        //private void SetEstimateZakatReturnsLayoutHeightWidth()
+        //{
+           
+        //    double HeightWidth = (DeviceWidth*87)/100/3;
+        //    EstimateZakatReturns.HeightRequest = HeightWidth;
+        //    EstimateZakatReturns.HeightRequest = HeightWidth;
+
+                
+        //}
+
+        //private void SetMyCertificatesLayoutHeightWidth()
+        //{
+        //          double HeightWidth = (DeviceWidth * (87 / 100)) / 3;
+        //    MyCertificates.HeightRequest = HeightWidth;
+        //    MyCertificates.HeightRequest = HeightWidth;
+
+
+        //}
+
+        //private void SetMyBillsInvoicesLayoutHeightWidth()
+        //{
+        //    double HeightWidth = (DeviceWidth * (87 / 100)) / 3;
+        //    MyBillsInvoices.HeightRequest = HeightWidth;
+        //    MyBillsInvoices.HeightRequest = HeightWidth;
+        //}
+
         void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
             SKImageInfo info = args.Info;
