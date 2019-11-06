048236436ec441a5b7614379b460a8461c7df6ad
Parag Bihade <parag.bihade@parallelminds.in>
2019-11-07 00:51:57 +0530

    updated TP and also updated XAML

--- GAZT/GAZT/Models/TaxPayerProfile.cs
+++ GAZT/GAZT/Models/TaxPayerProfile.cs
@@ -7,9 +7,62 @@ namespace GAZT.Models
     //ideally it should be UserId, TIN etc. but based on API responsethe naming of variable has been matched 
     public class TaxPayerProfile
     {
+        public String FirstName{ get; set; }
+        public String LastName { get; set; }
+        public String Name
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
         public String Email { get; set; }
+
+        private String _NewEmail = String.Empty;
+        public string NewEmail
+        {
+            get
+            {
+                return _NewEmail;
+            }
+            set
+            {
+                _NewEmail = value;
+            }
+        }
+
         public String Userid { get; set; }
         public String Tin { get; set; }
-        public String Mobile { get; set; }        
+        public String Mobile { get; set; }
+
+        private String _NewMobile = String.Empty;
+        public string NewMobile
+        {
+            get
+            {
+                return _NewMobile;
+            }
+            set
+            {
+                _NewMobile = value;
+            }
+        }
+
+
+        public String Password { get; set; }
+        private String _NewPassword = String.Empty;
+        public string NewPassword
+        {
+            get
+            {
+                return _NewPassword;
+            }
+            set
+            {
+                _NewPassword = value;
+            }
+        }
+
     }
 }
