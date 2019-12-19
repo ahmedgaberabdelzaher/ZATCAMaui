376be035b8c83aea32a5109e8820e79f16ee4c8f
Onkar Palange <Onkar.Palange@parallelminds.in>
2019-12-19 20:50:15 +0530

    MyBill Change in UI

--- GAZT/GAZT/ViewModel/MyBillsViewModel.cs
+++ GAZT/GAZT/ViewModel/MyBillsViewModel.cs
@@ -169,56 +169,56 @@ namespace GAZT.ViewModel
         public async void onPageLoad()
         {
 
-            List<MyBills> myBills = new List<MyBills>();
-            try
+            //List<MyBills> myBills = new List<MyBills>();
+            //try
+            //{
+
+
+            //    myBills = await WebServiceManager.GAZTGetMyBills(App.TP.Tin);
+
+            //    if (myBills.Count != 0)
+            //    {
+            //        MyBills = new List<MyBills>();
+            //        MyBills = myBills;
+            //        MyBillsOriginal = myBills;
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Device.BeginInvokeOnMainThread(async () =>
+            //    {
+            //        await _dialogService.ShowMessageBox("No Bills Found", AppResources.Information);
+            //    });
+            //}
+
+            int i = 5;
+            MyBills = new List<MyBills>();
+            for (i = 0; i < 6; i++)
             {
+                MyBills m = new MyBills();
+                m.Abtypt = "Abc";
+                m.BETRW = "100";
+                m.FAEDN = "12:02:20";
+                m.Status = "P";
+                m.VTRE2 = "54321";
 
-
-                myBills = await WebServiceManager.GAZTGetMyBills(App.TP.Tin);
-
-                if (myBills.Count != 0)
-                {
-                    MyBills = new List<MyBills>();
-                    MyBills = myBills;
-                    MyBillsOriginal = myBills;
-                }
+                MyBills.Add(m);
             }
-            catch (Exception e)
+
+            int j = 5;
+            //S MyBills = new List<MyBills>();
+            for (i = 0; i < 6; i++)
             {
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    await _dialogService.ShowMessageBox("No Bills Found", AppResources.Information);
-                });
+                MyBills m = new MyBills();
+                m.Abtypt = "Abc";
+                m.BETRW = "100";
+                m.FAEDN = "12:02:20";
+                m.Status = "I";
+                m.VTRE2 = "54321";
+
+                MyBills.Add(m);
             }
-
-            // int i = 5;
-            // MyBills = new List<MyBills>();
-            // for (i = 0; i < 6; i++)
-            // {
-            //     MyBills m = new MyBills();
-            //     m.Abtypt = "Abc";
-            //     m.BETRW = "100";
-            //     m.FAEDN = "12:02:20";
-            //     m.Status = "P";
-            //     m.VTRE2 = "54321";
-
-            //     MyBills.Add(m);
-            // }
-
-            // int j = 5;
-            ////S MyBills = new List<MyBills>();
-            // for (i = 0; i < 6; i++)
-            // {
-            //     MyBills m = new MyBills();
-            //     m.Abtypt = "Abc";
-            //     m.BETRW = "100";
-            //     m.FAEDN = "12:02:20";
-            //     m.Status = "I";
-            //     m.VTRE2 = "54321";
-
-            //     MyBills.Add(m);
-            // }
-            // MyBillsOriginal = MyBills;
+            MyBillsOriginal = MyBills;
         }
     }
 }
