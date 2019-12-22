25033c35a9d649187eb8cf81386f076c48f2e56d
Onkar Palange <Onkar.Palange@parallelminds.in>
2019-12-22 23:14:20 +0530

    SSL Certificate Problem solved

--- GAZT/GAZT/Manager/WebServiceManager.cs
+++ GAZT/GAZT/Manager/WebServiceManager.cs
@@ -945,7 +945,7 @@ namespace GAZT.Manager
             try
             {
 
-                HttpClient client = new HttpClient(new System.Net.Http.HttpClientHandler());
+                HttpClient client = new HttpClient(App.httpClientHandler);
                 // string uri = "https://tstdg1as1.mygazt.gov.sa:8080/sap/opu/odata/SAP/ZDP_FRGT_USRNM_PWD_SRV/HeaderSet(Tin='3102164652',EmailId='',TpType='',MobileNo='',SubType='',Idnumber='',Otp='',NewPwd='',RdBt='P',Dob=datetime'2015-07-05T15:13:49',Langu='E')?Saml2=disabled&$format=json";
                 string uri =Constants.FogotPasswordSendOTP + Tin +"'" + ",EmailId='" + Tin + "'"+ ",TpType='" + Tin + "'" + ",MobileNo='" + Tin + "'" + ",SubType='" + Tin + "'" + ",Idnumber='" + Tin + "'" + ",Otp='" + Tin +"'" + ",NewPwd='"+ Tin + "'" + ",RdBt='" +"P'" + ",Dob=datetime'"+ "2015 -07-05T15:13:49" + "'" + ",Langu='" + "E" + "'" +")?Saml2=disabled&$format=json";
 
