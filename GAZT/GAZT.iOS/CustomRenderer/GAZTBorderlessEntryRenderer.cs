95b96a403fcfda01f239d6f602b6abab80db1c2c
SantharaoT <santharao.t@parallelminds.in>
2019-12-22 07:29:35 +0530

    Crashing issue solved on ios for newly log in page

--- GAZT/GAZT.iOS/CustomRenderer/GAZTBorderlessEntryRenderer.cs
+++ GAZT/GAZT.iOS/CustomRenderer/GAZTBorderlessEntryRenderer.cs
@@ -20,20 +20,59 @@ namespace GAZT.iOS.CustomRenderer
     public class GAZTBorderlessEntryRenderer : EntryRenderer
     {
 
-       
+
+        public static void Init() { }
+        double fontSize;
+
+
 
         protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             base.OnElementPropertyChanged(sender, e);
+            if (Control != null)
+            {
 
-            Control.Layer.BorderWidth = 0;
-            Control.BorderStyle = UITextBorderStyle.None;
-            Control.TextColor = UIColor.Black;
+                if (App.IsArabic)
+                {
+                    Control.TextAlignment = UITextAlignment.Right;
+                }
+                else
+                {
+                    Control.TextAlignment = UITextAlignment.Left;
+                }
+                if (App.IsOTPiew)
+                {
+                    Control.TextAlignment = UITextAlignment.Center;
+                }
+                else
+                {
+                    if (App.IsArabic)
+                    {
+                        Control.TextAlignment = UITextAlignment.Right;
+                    }
+                    else
+                    {
+                        Control.TextAlignment = UITextAlignment.Left;
+                    }
+                }
+                //if (StyleId.Equals("OTPEntry"))
+                //{
+                //    Control.TextAlignment = UITextAlignment.Center;
+                //}
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.TextColor = UIColor.Black;
+            }
 
-
-           
+            fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+            //this.Control.Font = UIFont.FromName(NSBundle.MainBundle.LocalizedString("FontName", ""), (float)fontSize);
+            // this.Control.Font = UIFont.SystemFontOfSize((float)fontSize);
+            fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+            //if (App.IsArabic)
+            //    this.Control.Font = UIFont.FromName("Cairo-Regular", (float)fontSize);
+            //else
+            //this.Control.Font = UIFont.FromName("Helvetica-Normal", (float)fontSize);
 
         }
-
     }
 }
