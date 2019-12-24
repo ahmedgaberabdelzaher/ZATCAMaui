fae7565073a573c6d3080e30e8967771bfd530cc
Onkar Palange <Onkar.Palange@parallelminds.in>
2019-12-25 01:47:50 +0530

    Working on a New Otp page

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -70,7 +70,7 @@ namespace GAZT
             AppResources.Culture = ci;
 
             InitializeComponent();
-            CustomNavigation navigationPage = new CustomNavigation(new LogInView());
+            CustomNavigation navigationPage = new CustomNavigation(new OTPPageView());
 
             var navigationService = (NavigationService)ServiceLocator.Current.GetInstance<INavigationService>();
             navigationService.Initialize(navigationPage);
