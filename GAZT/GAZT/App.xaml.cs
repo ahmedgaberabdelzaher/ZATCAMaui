56a5e9f88378f38f0bf6400efe3f499b2a138251
Onkar Palange <Onkar.Palange@parallelminds.in>
2019-12-26 19:04:43 +0530

    Merge completed

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -72,7 +72,7 @@ namespace GAZT
             AppResources.Culture = ci;
 
             InitializeComponent();
-            CustomNavigation navigationPage = new CustomNavigation(new LogInPageView());
+            CustomNavigation navigationPage = new CustomNavigation(new LogInView());
 
             var navigationService = (NavigationService)ServiceLocator.Current.GetInstance<INavigationService>();
             navigationService.Initialize(navigationPage);
