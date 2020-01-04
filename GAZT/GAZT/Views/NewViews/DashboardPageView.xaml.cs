e518ac696874f0644250ff7b0397a66f0e39c871
Onkar Palange <Onkar.Palange@parallelminds.in>
2020-01-05 01:49:24 +0530

    Yesno Strings added

--- GAZT/GAZT/Views/NewViews/DashboardPageView.xaml.cs
+++ GAZT/GAZT/Views/NewViews/DashboardPageView.xaml.cs
@@ -95,7 +95,7 @@ namespace GAZT.Views
 
         private async void OnLogoutClicked(Object sender, EventArgs e)
         {
-            var result = await this.DisplayAlert("Alert!", AppResources.LogoutConfirmationMessage, "Yes", "No");
+            var result = await this.DisplayAlert("Alert!", AppResources.LogoutConfirmationMessage,AppResources.ZYes,AppResources.ZNo);
             if (result)
             {
                 App.TP = null;
