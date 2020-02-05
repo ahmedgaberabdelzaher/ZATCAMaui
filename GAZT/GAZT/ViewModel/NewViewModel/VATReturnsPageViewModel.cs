03b1a11947d2a401141b6e9f52704362437a2680
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-02-05 10:59:18 +0530

    Submit tested

--- GAZT/GAZT/ViewModel/NewViewModel/VATReturnsPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewViewModel/VATReturnsPageViewModel.cs
@@ -1307,6 +1307,7 @@ namespace GAZT.ViewModel.NewViewModel
         {
             ClearPage();
             IsVisibleInstrunction = true;
+            IsMainButtonEnabled = false;
             ButtonName = AppResources.ZVatStepTwo;
             
         }
@@ -1372,7 +1373,7 @@ namespace GAZT.ViewModel.NewViewModel
                 VATDeclarationData.d.Operationz = operation;
 
                 await SaveReturnAndGetReturnAndSetButtons();
-
+               _navigationService.NavigateTo(App.AcknowledgementDetailsPageView, VATDeclarationData);
             }
             else
             {
