96f63d08f4186e28042862f24c2503ea6280d84d
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-02-10 14:21:11 +0530

    Change refund

--- GAZT/GAZT/ViewModel/NewViewModel/LogInPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewViewModel/LogInPageViewModel.cs
@@ -31,11 +31,11 @@ namespace GAZT.ViewModel
         #endregion
         #region Property
 
-        private string _UserName = "3102226642";
+       // private string _UserName = "3102226642";
         //private string _UserName = "3102226709";
 
        // private string _UserName = "3100278126";
-       // private string _UserName = string.Empty;
+        private string _UserName = string.Empty;
         // private string _UserName = "3101965624";
         // private string _UserName = "3060210077";
        // private string _UserName = string.Empty;
@@ -81,9 +81,9 @@ namespace GAZT.ViewModel
         }
 
 
-        private string _Password = "Init@1234";
+      //  private string _Password = "Init@1234";
 
-      //private string _Password = string.Empty;
+      private string _Password = string.Empty;
         public string Password
         {
             get
