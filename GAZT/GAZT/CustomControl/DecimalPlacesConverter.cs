4bec7a7b000dddcc9a27028213e0d8717ddc86e4
Ashish Ranjanashish.ranjan@invenio-solutions.com <ashish.ranjan@invenio-solutions.com>
2020-02-18 16:12:17 +0530

    Crashing issue solved on VAT declaration on iOS which was coming due to multiple decimal entry

--- GAZT/GAZT/CustomControl/DecimalPlacesConverter.cs
+++ GAZT/GAZT/CustomControl/DecimalPlacesConverter.cs
@@ -9,6 +9,7 @@ namespace GAZT.CustomControl
 {
     public class DecimalPlacesConverter : Behavior<Entry>
     {
+         static int  decimalCount;
         protected override void OnAttachedTo(Entry entry)
         {
             entry.TextChanged += OnEntryTextChanged;
@@ -23,67 +24,190 @@ namespace GAZT.CustomControl
 
         private static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
         {
-            if (!string.IsNullOrEmpty(args.NewTextValue))
+            if (Device.RuntimePlatform == Device.iOS)
             {
-                
-                if (args.NewTextValue.Substring(args.NewTextValue.Length - 1) != ".")
+                GetDecimalCount(args.NewTextValue);
+                char LastChar = ' ';
+                if (!string.IsNullOrEmpty(args.NewTextValue))
                 {
-                    if (args.NewTextValue.Contains("."))
+                    char[] textValue = args.NewTextValue.ToCharArray();
+                     LastChar = textValue[textValue.Length - 1];
+                }
+
+                if(LastChar >= 46 && LastChar <= 57)
+                {
+
+
+                    if (DecimalPlacesConverter.decimalCount < 2)
                     {
-                        string[] SplitByDecimal = args.NewTextValue.Split('.');
-                        string BeforeDecimal = string.Empty;
-                        string AfterDecimal = string.Empty;
-                      
-                        if (SplitByDecimal[0].Length > 14)
+                        if (!string.IsNullOrEmpty(args.NewTextValue))
                         {
 
-                            BeforeDecimal = SplitByDecimal[0].Remove(SplitByDecimal[0].Length - 1);
-                            if(BeforeDecimal.Contains("-"))
+                            if (args.NewTextValue.Substring(args.NewTextValue.Length - 1) != ".")
                             {
-                                BeforeDecimal = (Convert.ToInt32(BeforeDecimal)*-1).ToString();
-                            }
-                            ((Entry)sender).Text = Math.Round(Convert.ToDecimal(BeforeDecimal + AfterDecimal), 2).ToString();
+                                if (args.NewTextValue.Contains("."))
+                                {
+                                    string[] SplitByDecimal = args.NewTextValue.Split('.');
+                                    string BeforeDecimal = string.Empty;
+                                    string AfterDecimal = string.Empty;
 
-                        }
-                        else
-                        {
-                            if (args.NewTextValue.Contains("-"))
-                            {
-                                ((Entry)sender).Text = (Math.Round(Convert.ToDecimal(args.NewTextValue), 2)*-1).ToString();
-                            }
-                            else
-                            {
-                                ((Entry)sender).Text = Math.Round(Convert.ToDecimal(args.NewTextValue), 2).ToString();
+                                    if (SplitByDecimal[0].Length > 14)
+                                    {
+
+                                        BeforeDecimal = SplitByDecimal[0].Remove(SplitByDecimal[0].Length - 1);
+                                        if (BeforeDecimal.Contains("-"))
+                                        {
+                                            BeforeDecimal = (Convert.ToInt32(BeforeDecimal) * -1).ToString();
+                                        }
+                                        ((Entry)sender).Text = Math.Round(Convert.ToDecimal(BeforeDecimal + AfterDecimal), 2).ToString();
+
+                                    }
+                                    else
+                                    {
+                                        if (args.NewTextValue.Contains("-"))
+                                        {
+                                            ((Entry)sender).Text = (Math.Round(Convert.ToDecimal(args.NewTextValue), 2) * -1).ToString();
+                                        }
+                                        else
+                                        {
+                                            ((Entry)sender).Text = Math.Round(Convert.ToDecimal(args.NewTextValue), 2).ToString();
+                                        }
+                                    }
+                                }
+                                else
+                                {
+
+
+                                    if (args.NewTextValue.Length > 14)
+                                    {
+                                        if (args.NewTextValue.Contains("-"))
+                                        {
+                                            ((Entry)sender).Text = (Math.Round(Convert.ToDecimal(args.NewTextValue.Remove(args.NewTextValue.Length - 1)), 2) * -1).ToString();
+                                        }
+                                        else
+                                        {
+                                            ((Entry)sender).Text = args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+                                        }
+
+
+                                    }
+                                    else
+                                    {
+                                        if (args.NewTextValue.Contains("-"))
+                                        {
+                                            ((Entry)sender).Text = args.NewTextValue.Replace("-", "");
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
-                    else
+                }
+                else
+                {
+                    if(!string.IsNullOrEmpty(args.NewTextValue))
+                    ((Entry)sender).Text = args.NewTextValue.Substring(0, args.NewTextValue.Length - 1).ToString();
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(args.NewTextValue))
+                {
+
+                    if (args.NewTextValue.Substring(args.NewTextValue.Length - 1) != ".")
                     {
-                        
-                        
-                        if (args.NewTextValue.Length > 14)
+                        if (args.NewTextValue.Contains("."))
                         {
-                            if (args.NewTextValue.Contains("-"))
+                            string[] SplitByDecimal = args.NewTextValue.Split('.');
+                            string BeforeDecimal = string.Empty;
+                            string AfterDecimal = string.Empty;
+
+                            if (SplitByDecimal[0].Length > 14)
                             {
-                                ((Entry)sender).Text = (Math.Round(Convert.ToDecimal(args.NewTextValue.Remove(args.NewTextValue.Length - 1)), 2) * -1).ToString();
+
+                                BeforeDecimal = SplitByDecimal[0].Remove(SplitByDecimal[0].Length - 1);
+                                if (BeforeDecimal.Contains("-"))
+                                {
+                                    BeforeDecimal = (Convert.ToInt32(BeforeDecimal) * -1).ToString();
+                                }
+                                ((Entry)sender).Text = Math.Round(Convert.ToDecimal(BeforeDecimal + AfterDecimal), 2).ToString();
+
                             }
                             else
                             {
-                                ((Entry)sender).Text = args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+                                if (args.NewTextValue.Contains("-"))
+                                {
+                                    ((Entry)sender).Text = (Math.Round(Convert.ToDecimal(args.NewTextValue), 2) * -1).ToString();
+                                }
+                                else
+                                {
+                                    ((Entry)sender).Text = Math.Round(Convert.ToDecimal(args.NewTextValue), 2).ToString();
+                                }
                             }
-                           
-                          
                         }
                         else
                         {
-                            if (args.NewTextValue.Contains("-"))
+
+
+                            if (args.NewTextValue.Length > 14)
                             {
-                                ((Entry)sender).Text = args.NewTextValue.Replace("-","");
+                                if (args.NewTextValue.Contains("-"))
+                                {
+                                    ((Entry)sender).Text = (Math.Round(Convert.ToDecimal(args.NewTextValue.Remove(args.NewTextValue.Length - 1)), 2) * -1).ToString();
+                                }
+                                else
+                                {
+                                    ((Entry)sender).Text = args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+                                }
+
+
+                            }
+                            else
+                            {
+                                if (args.NewTextValue.Contains("-"))
+                                {
+                                    ((Entry)sender).Text = args.NewTextValue.Replace("-", "");
+                                }
                             }
                         }
                     }
                 }
             }
+
+            
+        }
+
+        private static void GetDecimalCount( string DecimalNumber)
+        {
+            int _decimalcount = 0;
+            char[] decimalNumber = new char[20];
+            try
+            {
+                if(DecimalNumber != null)
+                {
+                     decimalNumber = DecimalNumber.ToCharArray();
+                }
+                
+                for (int i = 0; i < decimalNumber.Length; i++)
+                {
+                    if (decimalNumber[i].Equals('.'))
+                    {
+                        _decimalcount++;
+
+                    }
+
+                }
+                decimalCount = _decimalcount;
+                if (decimalCount > 1)
+                {
+                    // message please remove extra decimal number
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+           
         }
     }
 }
