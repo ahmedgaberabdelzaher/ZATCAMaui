8e04f7e996d93fed060f5ca1438fbbdee781f30f
Ashish Ranjanashish.ranjan@invenio-solutions.com <ashish.ranjan@invenio-solutions.com>
2020-02-18 22:25:02 +0530

    App version shown on iOS

--- GAZT/GAZT.iOS/AppDelegate.cs
+++ GAZT/GAZT.iOS/AppDelegate.cs
@@ -33,7 +33,7 @@ namespace GAZT.iOS
             Rg.Plugins.Popup.Popup.Init();
             IQKeyboardManager.SharedManager.Enable = true;
             UINavigationBar.Appearance.TintColor = UIColor.Red;
-            
+            App.AppVersion = NSBundle.MainBundle.InfoDictionary["CFBundleShortVersionString"].ToString();
             LoadApplication(new App());
 
             //Code for PUSH notification
