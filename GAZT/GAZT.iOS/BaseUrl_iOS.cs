91f65da6eb8eb11da307d811052ad9ef1390aba8
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-03-05 23:00:16 +0300

    Added image to the html content comming from api

---
+++ GAZT/GAZT.iOS/BaseUrl_iOS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Foundation;
+using UIKit;
+using GAZT.Views.NewViews;
+using GAZT.iOS;
+
+[assembly: Dependency(typeof(BaseUrl_iOS))]
+namespace GAZT.iOS
+{
+	public class BaseUrl_iOS : IBaseUrl
+	{
+		public string Get()
+		{
+			return NSBundle.MainBundle.BundlePath;
+		}
+	}
+}
