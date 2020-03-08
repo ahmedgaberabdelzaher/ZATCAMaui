96e9e922a25ee702b653a26ec42b3065aa525f18
Ashish Ranjanashish.ranjan@invenio-solutions.com <ashish.ranjan@invenio-solutions.com>
2020-03-08 19:39:36 +0530

    N/A related issue solved on Salesdetails page

--- GAZT/GAZT/ViewModel/NewViewModel/LogInPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewViewModel/LogInPageViewModel.cs
@@ -27,9 +27,11 @@ namespace GAZT.ViewModel
         #endregion
         #region Property
 
-        // private string _UserName = "3000028177";
+        // private string _UserName = "3000028177";// can test count match issue
         //private string _UserName = "3101591813";Jino provided
-        private string _UserName = "3101762303";
+        // private string _UserName = "3101762303";
+        private string _UserName = "3101710116";
+       
         //private string _UserName = string.Empty;
         public string UserName
         {
