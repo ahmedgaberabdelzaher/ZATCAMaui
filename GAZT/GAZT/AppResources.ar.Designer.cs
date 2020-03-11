6eee5972ca189c3ca42ac4eb1197df795ceb2abe
Ashish Ranjanashish.ranjan@invenio-solutions.com <ashish.ranjan@invenio-solutions.com>
2020-03-12 00:49:58 +0530

    Pulling latest changes

--- GAZT/GAZT/AppResources.ar.Designer.cs
+++ GAZT/GAZT/AppResources.ar.Designer.cs
@@ -4175,5 +4175,53 @@ namespace GAZT {
                 return ResourceManager.GetString("ZZYoushouldsignupasnewuser", resourceCulture);
             }
         }
+        
+        public static string ZZAccountCreatedSuccessfully {
+            get {
+                return ResourceManager.GetString("ZZAccountCreatedSuccessfully", resourceCulture);
+            }
+        }
+        
+        public static string ZZNewpasswordfieldandconfirmPasswordfieldshouldmatchup {
+            get {
+                return ResourceManager.GetString("ZZNewpasswordfieldandconfirmPasswordfieldshouldmatchup", resourceCulture);
+            }
+        }
+        
+        public static string ZZPleaseenterconfirmationcodesenttoyouremailaddress {
+            get {
+                return ResourceManager.GetString("ZZPleaseenterconfirmationcodesenttoyouremailaddress", resourceCulture);
+            }
+        }
+        
+        public static string ZZPleaseenterconfirmationcodesenttoyourmobilenumber {
+            get {
+                return ResourceManager.GetString("ZZPleaseenterconfirmationcodesenttoyourmobilenumber", resourceCulture);
+            }
+        }
+        
+        public static string ZZSignup {
+            get {
+                return ResourceManager.GetString("ZZSignup", resourceCulture);
+            }
+        }
+        
+        public static string ZZYourEntitytypeisIndividual {
+            get {
+                return ResourceManager.GetString("ZZYourEntitytypeisIndividual", resourceCulture);
+            }
+        }
+        
+        public static string ZZIBANisincorrect {
+            get {
+                return ResourceManager.GetString("ZZIBANisincorrect", resourceCulture);
+            }
+        }
+        
+        public static string ZZPleaseretainthisnumberforfuturereferenceandcommunucation {
+            get {
+                return ResourceManager.GetString("ZZPleaseretainthisnumberforfuturereferenceandcommunucation", resourceCulture);
+            }
+        }
     }
 }
