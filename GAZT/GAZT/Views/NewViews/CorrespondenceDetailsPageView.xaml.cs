8755535d21d8334a00ffddb37ea293aed06f13ab
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-03-11 17:51:30 +0530

    Corresponse image change

--- GAZT/GAZT/Views/NewViews/CorrespondenceDetailsPageView.xaml.cs
+++ GAZT/GAZT/Views/NewViews/CorrespondenceDetailsPageView.xaml.cs
@@ -70,13 +70,13 @@ namespace GAZT.Views.NewViews
 
             }
 
-            //string trim1 = HTMLContent.Replace("</body>", " ");
-            //string newHTMLContent = trim1.Replace("</html>", "<br><img src='ic_GAZT_Logo_Text.png' width='40%'/></<br><br><br><br><br><br><br><br><br></body></html>");
+            string trim1 = HTMLContent.Replace("</body>", " ");
+            string newHTMLContent = trim1.Replace("</html>", "<br><img src='ic_GAZT_Logo_Text.png' width='40%'/></<br><br><br><br><br><br><br><br><br></body></html>");
 
             var htmlSource = new HtmlWebViewSource();
 
-            /*htmlSource.Html = newHTMLContent*/;
-            htmlSource.Html = HTMLContent;
+            htmlSource.Html = newHTMLContent;
+           // htmlSource.Html = HTMLContent;
 
             htmlSource.BaseUrl = DependencyService.Get<IBaseUrl>().Get();
             CorWebView.Source = htmlSource;
