15d1a7246ed07272c988ab75d7b54644da1f68e2
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-03-24 23:01:07 +0530

    New Otp screen completed

---
+++ GAZT/GAZT/Helper/IStatusBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GAZT.Helper
+{
+    public interface IStatusBar
+    {
+        void HideStatusBar();
+        void ShowStatusBar();
+    }
+}
