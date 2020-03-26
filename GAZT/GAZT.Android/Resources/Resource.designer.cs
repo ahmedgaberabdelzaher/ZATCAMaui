c5a9ca463cf85c1465323dd5210b23fba77ed98c
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-03-27 04:16:04 +0530

    TINS status

--- GAZT/GAZT.Android/Resources/Resource.designer.cs
+++ GAZT/GAZT.Android/Resources/Resource.designer.cs
@@ -20623,31 +20623,40 @@ namespace GAZT.Droid
 			public const int sf_Submited_Returns = 2131165697;
 			
 			// aapt resource value: 0x7F070202
-			public const int sf_Unsubmited_Returns = 2131165698;
+			public const int sf_Tax_Evasion = 2131165698;
 			
 			// aapt resource value: 0x7F070203
-			public const int sf_VAT_Declaration = 2131165699;
+			public const int sf_TIN_Status = 2131165699;
 			
 			// aapt resource value: 0x7F070204
-			public const int splash = 2131165700;
+			public const int sf_Unsubmited_Returns = 2131165700;
 			
 			// aapt resource value: 0x7F070205
-			public const int TimeZone = 2131165701;
+			public const int sf_VAT_Declaration = 2131165701;
 			
 			// aapt resource value: 0x7F070206
-			public const int tooltip_frame_dark = 2131165702;
+			public const int sf_VAT_Lookup = 2131165702;
 			
 			// aapt resource value: 0x7F070207
-			public const int tooltip_frame_light = 2131165703;
+			public const int splash = 2131165703;
 			
 			// aapt resource value: 0x7F070208
-			public const int Untitled2 = 2131165704;
+			public const int TimeZone = 2131165704;
 			
 			// aapt resource value: 0x7F070209
-			public const int user = 2131165705;
+			public const int tooltip_frame_dark = 2131165705;
 			
 			// aapt resource value: 0x7F07020A
-			public const int ViewMode = 2131165706;
+			public const int tooltip_frame_light = 2131165706;
+			
+			// aapt resource value: 0x7F07020B
+			public const int Untitled2 = 2131165707;
+			
+			// aapt resource value: 0x7F07020C
+			public const int user = 2131165708;
+			
+			// aapt resource value: 0x7F07020D
+			public const int ViewMode = 2131165709;
 			
 			static Drawable()
 			{
--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LandingPage/SFLandingPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LandingPage/SFLandingPageViewModel.cs
@@ -707,7 +707,7 @@ private void ShowOptionsCommandClicked(object obj)
            
             eServicesAvailableToTheTP.Add(new eServiceInfo { eServiceName = AppResources.MyBills, BackgroundGradientStart = "#006450", BackgroundGradientEnd = "#CCE0DC", iConImagePath = "sf_My_Bills.png", OnClickEvents = "OnTappedBills" });
             eServicesAvailableToTheTP.Add(new eServiceInfo { eServiceName = AppResources.MyCertificate, BackgroundGradientStart = "#006450", BackgroundGradientEnd = "#CCE0DC", iConImagePath = "sf_My_Certificate.png", OnClickEvents= "OnTappedCertificate" });
-            eServicesAvailableToTheTP.Add(new eServiceInfo { eServiceName =  AppResources.ZTINStatus, BackgroundGradientStart = "#006450", BackgroundGradientEnd = "#CCE0DC", iConImagePath = "sf_Estimated_Zakat_Returns.png", OnClickEvents = "OnTappedTINStatus" });
+            eServicesAvailableToTheTP.Add(new eServiceInfo { eServiceName =  AppResources.ZTINStatus, BackgroundGradientStart = "#006450", BackgroundGradientEnd = "#CCE0DC", iConImagePath = "sf_TIN_Status.png", OnClickEvents = "OnTappedTINStatus" });
             eServicesAvailableToTheTP.Add(new eServiceInfo { eServiceName = AppResources.ZZCorrespondence, BackgroundGradientStart = "#006450", BackgroundGradientEnd = "#CCE0DC", iConImagePath = "sf_Correspondence.png", OnClickEvents = "OnTappedCorrespondence" });
             eServicesAvailableToTheTP.Add(new eServiceInfo { eServiceName = AppResources.ZTEReportReportScreenTitle, BackgroundGradientStart = "#006450", BackgroundGradientEnd = "#CCE0DC", iConImagePath = "sf_Form_Bundle_Status.png", OnClickEvents = "OnTappedCorrespondence" });
         }
