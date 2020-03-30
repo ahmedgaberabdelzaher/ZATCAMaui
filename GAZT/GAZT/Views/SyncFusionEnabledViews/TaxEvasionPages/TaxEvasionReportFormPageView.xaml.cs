dd5e0c78aa4769e3b7ac863e1ce311df6962b71d
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-03-31 00:18:49 +0300

    picker manupilation ar en

--- GAZT/GAZT/Views/SyncFusionEnabledViews/TaxEvasionPages/TaxEvasionReportFormPageView.xaml.cs
+++ GAZT/GAZT/Views/SyncFusionEnabledViews/TaxEvasionPages/TaxEvasionReportFormPageView.xaml.cs
@@ -169,6 +169,7 @@ namespace GAZT.Views.NewViews
                 this.FlowDirection = FlowDirection.LeftToRight;
                 CultureInfo.CurrentUICulture = new CultureInfo("en-US");
                 Thread.CurrentThread.CurrentUICulture = CultureInfo.CurrentUICulture;
+                PickerResourceManager.Manager = new ResourceManager("GAZT.AppResources", Application.Current.GetType().Assembly);
                 viewModel.IsVisiblePickerAr = false;
                 viewModel.IsVisiblePickerEn = true;
             }
