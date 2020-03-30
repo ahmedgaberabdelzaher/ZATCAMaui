cbaa842fc0434679627ca1e39a988b11b4a7e0aa
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-03-30 07:27:18 +0530

    Crash issue solved

--- GAZT/GAZT/Views/SyncFusionEnabledViews/LandingPage/SFAnonymousLandingPageView.xaml.cs
+++ GAZT/GAZT/Views/SyncFusionEnabledViews/LandingPage/SFAnonymousLandingPageView.xaml.cs
@@ -167,7 +167,7 @@ namespace GAZTeServicesApp.Views.LandingPage
 
                 if (BModel.eServiceName == AppResources.ZTEReportReportScreenTitle)
                 {
-                    viewModel._navigationService.NavigateTo(App.TaxEvasionReportListPageView, App.TaxEvasionReportListPageView);
+                    viewModel._navigationService.NavigateTo(App.TaxEvasionReportListPageView);
                 }
 
                 if (BModel.eServiceName == AppResources.VATLookup)
