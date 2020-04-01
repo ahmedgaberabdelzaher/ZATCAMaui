0636f43ec3fb839cfcc13d0776df9782c179db63
Parag Bihade <Parag.Bihade@parallelminds.in>
2020-04-02 00:20:59 +0300

    addedtest crash

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -303,6 +303,8 @@ namespace GAZT
                   "uwp={Your UWP App secret here};" +
                   "android={c4abea0b-7d25-4680-9354-b0c3e4b2fb7a}",
                   typeof(Analytics), typeof(Crashes), typeof(Distribute));
+
+            Crashes.GenerateTestCrash();
         }
 
         bool OnReleaseAvailable(ReleaseDetails releaseDetails)
