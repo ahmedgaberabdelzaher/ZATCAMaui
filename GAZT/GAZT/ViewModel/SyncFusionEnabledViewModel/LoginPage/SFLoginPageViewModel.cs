40899df0e6dbace1f1648c32c5bd247aaf2ffc8d
SantharaoT <ashish.ranjan@invenio-solutions.com>
2020-04-13 00:16:22 +0530

    Correspondance list view issue solved

--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
@@ -20,12 +20,12 @@ namespace GAZTeServicesApp.ViewModels.LoginPage
     {
         #region Fields
 
-        private string password = "Init@123";
-        private string email = "3102289204";
+        //private string password = "Init@123";
+        //private string email = "3102289204";
         // private string email = "3102285896";
 
-        //private string password;
-        //private string email;
+        private string password;
+        private string email;
         public int CurrentAttempt = 0;
         public ICommand BackButtonClicked { get; set; }
         private DateTime lastTapped;
