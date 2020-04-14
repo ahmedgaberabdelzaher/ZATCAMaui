01369fbc73a40751b0780332241dfc2265348a60
SantharaoT <ashish.ranjan@invenio-solutions.com>
2020-04-14 21:59:41 +0530

    Loader added on Salesdetails and Bills Details page

--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
@@ -21,7 +21,7 @@ namespace GAZTeServicesApp.ViewModels.LoginPage
         #region Fields
 
         private string password = "Init@123";
-        private string email = "3102289204";
+        private string email = "3102190365";
        // private string email = "3102232232";
 
         //private string password;
