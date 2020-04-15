45700ffbee822b5bded38f9cf596c629e5ff6852
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-04-16 01:06:09 +0300

    tax payer profile page landscape

--- GAZT/GAZT/Views/SyncFusionEnabledViews/TaxPayerProfilePages/TaxPayerProfilePageView.xaml.cs
+++ GAZT/GAZT/Views/SyncFusionEnabledViews/TaxPayerProfilePages/TaxPayerProfilePageView.xaml.cs
@@ -16,6 +16,8 @@ namespace GAZT.Views.NewViews
     {
         #region Variable
         TaxPayerProfilePageViewModel viewModel;
+        private double width = 0;
+      private double height = 0;
         #endregion
 
         #region Constructor
@@ -35,6 +37,26 @@ namespace GAZT.Views.NewViews
 
         #region Method
 
+
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (width != this.width || height != this.height)
+            {
+                this.width = width;
+                this.height = height;
+                if (width > height)
+                {
+                    this.BackgroundImageSource = "sf_LoginBackgroundLand.png";
+                }
+                else
+                {
+                    this.BackgroundImageSource = "sf_LoginBackground.png";
+                    //  outerStack.Orientation = StackOrientation.Vertical;
+                }
+            }
+        }
         public void ChangeAeroIcon()
         {
             if (App.IsArabic)
