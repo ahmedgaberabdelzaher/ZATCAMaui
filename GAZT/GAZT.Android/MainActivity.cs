a0441b08b2ac51a59571fe8b92511865fc2b190e
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-04-22 01:30:06 +0300

    changes qr scanner

--- GAZT/GAZT.Android/MainActivity.cs
+++ GAZT/GAZT.Android/MainActivity.cs
@@ -57,6 +57,7 @@ namespace GAZT.Droid
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
