bfb0fa434d68efd37d2a9be85a45a3cd9e9461f2
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-04-23 00:14:26 +0300

    Taxpayer name shown properly on landning page

--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
@@ -20,8 +20,8 @@ namespace GAZTeServicesApp.ViewModels.LoginPage
     {
         #region Fields
 
-        private string password = "Unit@123";
-        private string email = "3102290567";
+        private string password = "Init@123";
+        private string email = "3102290395";
         //private string password = "Init@123";
         //// private string email = "3102289044";
          //private string email = "3102289204";
