9e9239b150ce7caa96f55d559a7d44ba73c15e83
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-05-07 21:58:40 +0300

    SAR text added after each amount on ZAKAT and also fixed the issue related to status bar on Anonymous page

--- GAZT/GAZT.iOS/AppDelegate.cs
+++ GAZT/GAZT.iOS/AppDelegate.cs
@@ -44,7 +44,7 @@ namespace GAZT.iOS
             InitRoundedCornerView.Init();
             Rg.Plugins.Popup.Popup.Init();
             IQKeyboardManager.SharedManager.Enable = true;
-            UINavigationBar.Appearance.TintColor = UIColor.Red;
+          //  UINavigationBar.Appearance.TintColor = UIColor.Red;
             App.AppVersion = NSBundle.MainBundle.InfoDictionary["CFBundleShortVersionString"].ToString();
             App iosapp = new App();
             App.appObj = iosapp;
@@ -68,6 +68,13 @@ namespace GAZT.iOS
             Syncfusion.SfPdfViewer.XForms.iOS.SfPdfDocumentViewRenderer.Init();
             Syncfusion.SfRangeSlider.XForms.iOS.SfRangeSliderRenderer.Init();
             Distribute.DontCheckForUpdatesInDebug();
+            UIView statusBar = UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) as UIView;
+            if (statusBar != null && statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
+            {
+                statusBar.BackgroundColor = UIColor.Clear;// Color.FromHex("#7f6550").ToUIColor(); // change to your desired color 
+                                                          // statusBar.TintColor = UIColor.Yellow;
+
+            }
             LoadApplication(iosapp);
             //Code for PUSH notification
             //UIView statusBar = UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) as UIView;
