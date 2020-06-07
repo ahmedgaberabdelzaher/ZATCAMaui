aa3cced437aa964625b16993367a58706a62ff0a
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-06-07 17:18:00 +0300

    App is pointed to Production server

--- GAZT/GAZT/Helper/Constants.cs
+++ GAZT/GAZT/Helper/Constants.cs
@@ -44,9 +44,9 @@ namespace GAZT.Helper
         //GAZTGetEstimatedZakatReturnSADADNumber
         public static bool IsProductionRelease = false;
 
-        public static string BaseUrlOfODataServices = QABaseUrlForODataServices;
-        public static string BaseUrlOfAuthentication = QABaseUrlForAuthentication;
-        public static string DomainUrlForCookies = QADomainForCookies;
+        public static string BaseUrlOfODataServices = ProdBaseUrlForODataServices;
+        public static string BaseUrlOfAuthentication = ProdBaseUrlForAuthentication;
+        public static string DomainUrlForCookies = ProdDomainForCookies;
 
        
         public static string GAZTSAMLLoginServicePart = "/SAP/ZTP_ACCOUNT_SRV/GetInfoSet";
