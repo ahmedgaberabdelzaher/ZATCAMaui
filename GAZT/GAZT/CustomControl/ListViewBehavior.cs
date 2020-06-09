5e74b746bc8b93b590fc5b7cced8e41805bf15ad
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-06-09 18:47:34 +0300

    expanders white space list

---
+++ GAZT/GAZT/CustomControl/ListViewBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+
+
+namespace EGAZT.CustomControl
+{
+
+    public class ListViewBehavior : Behavior<SfListView>
+    {
+        private SfListView listView;
+        protected override void OnAttachedTo(SfListView bindable)
+        {
+            listView = bindable;
+            listView.Loaded += OnListViewLoaded;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void OnListViewLoaded(object sender, ListViewLoadedEventArgs e)
+        {
+            var container = listView.GetVisualContainer();
+            var extent = (double)container.GetType().GetRuntimeProperties().FirstOrDefault(x => x.Name == "TotalExtent").GetValue(container);
+            listView.HeightRequest = extent;
+        }
+
+        protected override void OnDetachingFrom(SfListView bindable)
+        {
+            listView.Loaded -= OnListViewLoaded;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
