a3e691164c45a7e71229e36b8069f06bbbbac05e
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-06-13 23:44:36 +0530

    Constant changed

--- GAZT/GAZT/Helper/Constants.cs
+++ GAZT/GAZT/Helper/Constants.cs
@@ -40,9 +40,9 @@ namespace GAZT.Helper
         //public static string BaseUrlOfAuthentication = QABaseUrlForAuthentication;
         //public static string DomainUrlForCookies = QADomainForCookies;
 
-        public static string BaseUrlOfODataServices = PreProdBaseUrlForODataServices;
-        public static string BaseUrlOfAuthentication = PreProdBaseUrlForAuthentication;
-        public static string DomainUrlForCookies = PreprodDomainForCookies;
+        public static string BaseUrlOfODataServices = QABaseUrlForODataServices;
+        public static string BaseUrlOfAuthentication = QABaseUrlForAuthentication;
+        public static string DomainUrlForCookies = QADomainForCookies;
 
         public static string GAZTSAMLLoginServicePart = "/SAP/ZTP_ACCOUNT_SRV/GetInfoSet";
         public static string GAZTSAMLLoginService = BaseUrlOfODataServices + "/sap/opu/odata/SAP/ZTP_ACCOUNT_SRV/GetInfoSet";
