23eeda4592510b87535e17a6ef9e4d8fdca19fef
divya.jannapureddy <divya.jannapureddy@invenio-solutions.com>
2020-06-22 19:53:27 +0530

    Adding Missing file for RealEstate fetaure

---
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/VATRealEstatePage/PropertyRegistrationPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using Xamarin.Forms;
+
+namespace EGAZT.ViewModel.SyncFusionEnabledViewModel.VATRealEstatePage
+{
+    public class PropertyRegistrationPageViewModel : ViewModelBase
+    {
+        public readonly INavigationService _navigationService;
+        public readonly IDialogService _dialogService;
+        public ICommand GoBackClick { get; set; }
+
+
+        private string _webUrl = string.Empty;
+        public string WebUrl
+        {
+            get
+            {
+                return _webUrl;
+            }
+            set
+            {
+                _webUrl = value;
+                RaisePropertyChanged("WebUrl");
+            }
+        }
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+            set
+            {
+                _isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+        public PropertyRegistrationPageViewModel(INavigationService navigationService, IDialogService dialogService)
+        {
+            if (navigationService == null)
+            {
+                throw new ArgumentNullException("navigationService");
+            }
+            _navigationService = navigationService;
+            if (dialogService == null)
+            {
+                throw new ArgumentNullException("dialogService");
+            }
+            _dialogService = dialogService;
+            GoBackClick = new Xamarin.Forms.Command(() =>
+            {
+                _navigationService.GoBack();
+            });
+        }
+    }
+}
+
