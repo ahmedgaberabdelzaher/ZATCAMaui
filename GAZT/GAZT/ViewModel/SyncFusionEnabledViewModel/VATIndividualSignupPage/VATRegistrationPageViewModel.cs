f899c40f835d4b8c9a6879d5a7549ace514d3fbc
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-06-26 06:51:30 +0530

    VAT Registration Ui Changed

--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/VATIndividualSignupPage/VATRegistrationPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/VATIndividualSignupPage/VATRegistrationPageViewModel.cs
@@ -11,6 +11,103 @@ namespace EGAZT.ViewModel.SyncFusionEnabledViewModel.VATIndividualSignupPage
         public readonly INavigationService _navigationService;
         public readonly IDialogService _dialogService;
 
+        #region Properties
+        private bool _isInstrunctionVisible = false;
+        public bool IsInstrunctionVisible
+        {
+            get
+            {
+                return _isInstrunctionVisible;
+            }
+            set
+            {
+                _isInstrunctionVisible = value;
+                RaisePropertyChanged("IsInstrunctionVisible");
+            }
+        }
+        private bool _isTaxPayersVisible = false;
+        public bool IsTaxPayersVisible
+        {
+            get
+            {
+                return _isTaxPayersVisible;
+            }
+            set
+            {
+                _isTaxPayersVisible = value;
+                RaisePropertyChanged("IsTaxPayersVisible");
+            }
+        }
+        private bool _isSalesVisible = false;
+        public bool IsSalesVisible
+        {
+            get
+            {
+                return _isSalesVisible;
+            }
+            set
+            {
+                _isSalesVisible = value;
+                RaisePropertyChanged("IsSalesVisible");
+            }
+        }
+        private bool _isExpensesVisible = false;
+        public bool IsExpensesVisible
+        {
+            get
+            {
+                return _isExpensesVisible;
+            }
+            set
+            {
+                _isExpensesVisible = value;
+                RaisePropertyChanged("IsExpensesVisible");
+            }
+        }
+        private bool _isFinancialVisible = false;
+        public bool IsFinancialVisible
+        {
+            get
+            {
+                return _isFinancialVisible;
+            }
+            set
+            {
+                _isFinancialVisible = value;
+                RaisePropertyChanged("IsFinancialVisible");
+            }
+        }
+        private bool _isSummaryVisible = false;
+        public bool IsSummaryVisible
+        {
+            get
+            {
+                return _isSummaryVisible;
+            }
+            set
+            {
+                _isSummaryVisible = value;
+                RaisePropertyChanged("IsSummaryVisible");
+            }
+        }
+
+        private String _currentStep;
+        public String CurrentStep
+        {
+            get
+            {
+                return _currentStep;
+            }
+            set
+            {
+                _currentStep = value;
+                RaisePropertyChanged("CurrentStep");
+            }
+        }
+
+
+        #endregion
+
         public VATRegistrationPageViewModel(INavigationService navigationService, IDialogService dialogService)
         {
             if (navigationService == null)
@@ -26,5 +123,20 @@ namespace EGAZT.ViewModel.SyncFusionEnabledViewModel.VATIndividualSignupPage
             _dialogService = dialogService;
 
         }
+
+        #region Method
+
+        public void SetVisibility()
+        {
+            IsInstrunctionVisible = false;
+            IsTaxPayersVisible = false;
+            IsSalesVisible = false;
+            IsExpensesVisible = false;
+            IsFinancialVisible = false;
+            IsSummaryVisible = false;
+        }
+
+        #endregion
+
     }
 }
