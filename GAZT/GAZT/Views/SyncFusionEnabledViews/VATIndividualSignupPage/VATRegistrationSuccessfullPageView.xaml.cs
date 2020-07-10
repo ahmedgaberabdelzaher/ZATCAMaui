da2611b63f1b1ab2887f3c534c71ae7e23a69555
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-07-11 00:38:29 +0300

    completed the registration flow

--- GAZT/GAZT/Views/SyncFusionEnabledViews/VATIndividualSignupPage/VATRegistrationSuccessfullPageView.xaml.cs
+++ GAZT/GAZT/Views/SyncFusionEnabledViews/VATIndividualSignupPage/VATRegistrationSuccessfullPageView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using EGAZT.ViewModel.SyncFusionEnabledViewModel.VATIndividualSignupPage;
+﻿using EGAZT.Models;
+using EGAZT.ViewModel.SyncFusionEnabledViewModel.VATIndividualSignupPage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace EGAZT.Views.SyncFusionEnabledViews.VATIndividualSignupPage
     public partial class VATRegistrationSuccessfullPageView : ContentPage
     {
         VATRegistrationSuccessfullPageViewModel viewModel;
-        public VATRegistrationSuccessfullPageView()
+        public VATRegistrationSuccessfullPageView(VATRegistrationDetails response)
         {
             InitializeComponent();
             viewModel = App.Locator.VATRegistrationSuccessfullPageView;
