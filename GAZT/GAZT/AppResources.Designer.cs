c237f617b6eac8697ba7e47dd27e2d935f614a75
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-07-12 08:57:48 +0300

    string checked

--- GAZT/GAZT/AppResources.Designer.cs
+++ GAZT/GAZT/AppResources.Designer.cs
@@ -2235,7 +2235,7 @@ namespace EGAZT {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Import or  Exporter.
+        ///   Looks up a localized string similar to Import  Exporter.
         /// </summary>
         public static string VATRImportExport {
             get {
