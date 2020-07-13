33be3ac4cb0e38e53e97fae2815180602ce74466
Parag Bihade <Parag.Bihade@parallelminds.in>
2020-07-13 18:36:11 +0300

    addd on boarding pages kept ready in anticipation

---
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/Onboarding/OnBoardingAnimationPageViewModel.cs
@@ -0,0 +1,228 @@
+﻿using EGAZT.Views.SyncFusionEnabledViews.OnBoarding;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Views;
+using GAZT.Models;
+using Syncfusion.SfRotator.XForms;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace EGAZT.ViewModel.SyncFusionEnabledViewModel.OnBoardingAnimation
+{
+    /// <summary>
+    /// ViewModel for on-boarding gradient page with animation.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    public class OnBoardingAnimationPageViewModel :ViewModelBase
+    {
+        #region Fields
+
+        public readonly INavigationService _navigationService;
+        public readonly IDialogService _dialogService;
+
+        private ObservableCollection<Boarding> boardings;
+
+        private string nextButtonText = "NEXT";
+
+        private bool isSkipButtonVisible = true;
+
+        private int selectedIndex;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance for the <see cref="OnBoardingAnimationPageViewModel" /> class.
+        /// </summary>
+        public OnBoardingAnimationPageViewModel(INavigationService navigationService, IDialogService dialogService)
+        {
+            if (navigationService == null)
+            {
+                throw new ArgumentNullException("navigationService");
+            }
+            _navigationService = navigationService;
+            _dialogService = dialogService;
+            if (dialogService == null)
+            {
+                throw new ArgumentNullException("dialogService");
+            }
+
+            this.SkipCommand = new Command(this.Skip);
+            this.NextCommand = new Command(this.Next);
+            this.Boardings = new ObservableCollection<Boarding>
+            {
+                new Boarding()
+                {
+                    ImagePath = "ReSchedule.png",
+                    Header = "COMMITMENTS",
+                    Content = "easy information on your obligations (payments and returns)",
+                    RotatorItem = new WalkthroughItemPage()
+                },
+                new Boarding()
+                {
+                    ImagePath = "ViewMode.png",
+                    Header = "RETURNS",
+                    Content = "VAT, ZAKAT and other return submissions made easy",
+                    RotatorItem = new WalkthroughItemPage()
+                },
+                new Boarding()
+                {
+                    ImagePath = "TimeZone.png",
+                    Header = "CORRESPONDENCE",
+                    Content = "keep track of your communication with GAZT",
+                    RotatorItem = new WalkthroughItemPage()
+                }
+            };
+
+            // Set bindingcontext to content view.
+            foreach (var boarding in this.Boardings)
+            {
+                boarding.RotatorItem.BindingContext = boarding;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Boarding> Boardings
+        {
+            get
+            {
+                return this.boardings;
+            }
+
+            set
+            {
+                if (this.boardings == value)
+                {
+                    return;
+                }
+
+                this.boardings = value;
+                RaisePropertyChanged("Boardings");
+            }
+        }
+
+        public string NextButtonText
+        {
+            get
+            {
+                return this.nextButtonText;
+            }
+
+            set
+            {
+                if (this.nextButtonText == value)
+                {
+                    return;
+                }
+
+                this.nextButtonText = value;
+                RaisePropertyChanged("NextButtonText");
+            }
+        }
+
+        public bool IsSkipButtonVisible
+        {
+            get
+            {
+                return this.isSkipButtonVisible;
+            }
+
+            set
+            {
+                if (this.isSkipButtonVisible == value)
+                {
+                    return;
+                }
+
+                this.isSkipButtonVisible = value;
+                RaisePropertyChanged("IsSkipButtonVisible");
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                return this.selectedIndex;
+            }
+
+            set
+            {
+                if (this.selectedIndex == value)
+                {
+                    return;
+                }
+
+                this.selectedIndex = value;
+                RaisePropertyChanged("SelectedIndex");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        /// <summary>
+        /// Gets or sets the command that is executed when the Skip button is clicked.
+        /// </summary>
+        public ICommand SkipCommand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command that is executed when the Done button is clicked.
+        /// </summary>
+        public ICommand NextCommand { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private bool ValidateAndUpdateSelectedIndex(int itemCount)
+        {
+            if (this.SelectedIndex >= itemCount - 1)
+            {
+                return true;
+            }
+
+            this.SelectedIndex++;
+            return false;
+        }
+
+        /// <summary>
+        /// Invoked when the Skip button is clicked.
+        /// </summary>
+        /// <param name="obj">The Object</param>
+        private void Skip(object obj)
+        {
+            this.MoveToNextPage();
+        }
+
+        /// <summary>
+        /// Invoked when the Done button is clicked.
+        /// </summary>
+        /// <param name="obj">The Object</param>
+        private void Next(object obj)
+        {
+            var itemCount = (obj as SfRotator).ItemsSource.Count();
+            if (this.ValidateAndUpdateSelectedIndex(itemCount))
+            {
+                this.MoveToNextPage();
+            }
+        }
+
+        private void MoveToNextPage()
+        {
+            _navigationService.NavigateTo(App.SFAnonymousLandingPageView);
+
+            //Application.Current.MainPage.Navigation.PopAsync();
+        }
+
+        #endregion
+    }
+}
