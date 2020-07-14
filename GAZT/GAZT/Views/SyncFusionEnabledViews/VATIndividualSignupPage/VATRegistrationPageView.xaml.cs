32174084d28f5f1165ae2f474560e674f925b89c
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-07-14 08:45:33 +0300

    layout handelded

--- GAZT/GAZT/Views/SyncFusionEnabledViews/VATIndividualSignupPage/VATRegistrationPageView.xaml.cs
+++ GAZT/GAZT/Views/SyncFusionEnabledViews/VATIndividualSignupPage/VATRegistrationPageView.xaml.cs
@@ -1570,7 +1570,8 @@ namespace EGAZT.Views.SyncFusionEnabledViews.VATIndividualSignupPage
 
         private void OnPageSelectedForIban(object sender, SelectionChangedEventArgs e)
         {
-
+                ((Xamarin.Forms.ListView)sender).SelectedItem = null;
+         
         }
 
         private void DateEntry_Focused_1(object sender, FocusEventArgs e)
@@ -2502,6 +2503,7 @@ namespace EGAZT.Views.SyncFusionEnabledViews.VATIndividualSignupPage
                     viewModel.MobNumberFR = vATSignUpData.d.Mobile.Substring(5);
                     viewModel.IdnumberFR = vATSignUpData.d.Idnum;
                     viewModel.SmtpAddrFR = vATSignUpData.d.Email;
+                    //viewModel.TypeFR = viewModel.SelectedIdTypeFR.Name
                    // vATSignUpData.d.Idtype
                     FrmTINNumber.HasError = false;
                 }
