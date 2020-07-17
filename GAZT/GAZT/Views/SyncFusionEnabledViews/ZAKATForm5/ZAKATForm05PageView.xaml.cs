45e18fa616505159947392f5af37795e32fd0022
Parag bihade <parag.bihade@invenio-solutions.com>
2020-07-17 21:13:47 +0530

    Added FORM5

---
+++ GAZT/GAZT/Views/SyncFusionEnabledViews/ZAKATForm5/ZAKATForm05PageView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.SyncFusionEnabledViews.ZAKATForm5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ZAKATForm05PageView : ContentPage
+    {
+        public ZAKATForm05PageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
