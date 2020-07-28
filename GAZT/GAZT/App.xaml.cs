c913cf3f318089c32ab3110cb11d124f95fd0a09
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-07-28 11:52:53 +0530

    Added iOS specific assets

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -185,7 +185,7 @@ namespace EGAZT
 
             ActivityIndicatorView = new ActivityIndicatorPageView();
             VATDeclaration vAT = null;
-            CustomNavigation navigationPage = new CustomNavigation(new StyleTestUIPageView()) { BarTextColor = Color.White };
+            CustomNavigation navigationPage = new CustomNavigation(new SFAnonymousLandingPageView()) { BarTextColor = Color.White };
             //CustomNavigation navigationPage = new CustomNavigation(new VATRegistrationPageView());
             //new NavigationPage(YouPage) { BarBackgroundColor = Color.White }
             var navigationService = (NavigationService)ServiceLocator.Current.GetInstance<INavigationService>();
