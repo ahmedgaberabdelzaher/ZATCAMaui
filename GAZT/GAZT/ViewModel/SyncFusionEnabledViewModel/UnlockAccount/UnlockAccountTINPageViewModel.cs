604c86c89e2e690bc87101aa3614d405b85d84d1
divya.jannapureddy <taqoxhpr3g744iuxwzz4nkmu3jbsiuecz3n4iqtdnrxtd2g6albq>
2020-07-29 16:40:24 +0530

    Fixed the otp issue.

--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/UnlockAccount/UnlockAccountTINPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/UnlockAccount/UnlockAccountTINPageViewModel.cs
@@ -551,6 +551,7 @@ namespace EGAZT.ViewModel.SyncFusionEnabledViewModel.UnlockAccount
 
             IsVerifyOTPEnabled = true;
             IsResendOTPEnabled = false;
+            IsVerifyOTPEnabled = true;
 
             VerifyButtonDisableColor = Color.FromHex("#d99b29");
             ButtonDisableColor = Color.FromHex("#9EA4A9");
