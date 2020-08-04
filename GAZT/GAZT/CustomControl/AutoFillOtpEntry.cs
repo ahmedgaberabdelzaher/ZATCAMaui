52e97815185c9d8808c6106666649acf8877af98
rohithkunchamwar <rohith.kunchamwar@invenio-solutions.com>
2020-08-04 17:25:20 +0530

    navigation for unlock account close button is handled.

---
+++ GAZT/GAZT/CustomControl/AutoFillOtpEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace EGAZT.CustomControl
+{
+    public class AutoFillOtpEntry:Entry
+    {
+        public AutoFillOtpEntry()
+        {
+
+        }
+    }
+}
