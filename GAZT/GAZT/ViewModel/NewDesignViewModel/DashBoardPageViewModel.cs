841272f0e85c010ccc68f594d75039db6267f89f
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-08-05 14:20:29 +0530

    Change in onboarding screen

--- GAZT/GAZT/ViewModel/NewDesignViewModel/DashBoardPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/DashBoardPageViewModel.cs
@@ -1041,7 +1041,8 @@ namespace EGAZT.ViewModel.NewDesignViewModel
 
             }
 
-            _navigationService.NavigateTo(App.GAZTNewDesignOnBoardingAnimationPageView);
+            //_navigationService.NavigateTo(App.GAZTNewDesignOnBoardingAnimationPageView);
+            await _navigation.PopToRootAsync();
             _navigation.NavigationStack.ToList().Clear();
         }
         #endregion
