3d07476c5504f4353d19826951d36b6af89e93c2
Onkar Palange <onkar.palange@invenio-solutions.com>
2020-08-11 20:35:35 +0530

    App resource integrated in NewUI

--- GAZT/GAZT/ViewModel/NewDesignViewModel/OnBoardingAnimationPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/OnBoardingAnimationPageViewModel.cs
@@ -304,7 +304,7 @@ namespace EGAZT.ViewModel.NewDesignViewModel.OnBoardingAnimation
 
         private void MoveToNextPage()
         {
-           _navigationService.NavigateTo(App.SFLoginPageView, App.GAZTNewDesignDashBoardPageView);
+           _navigationService.NavigateTo(App.GAZTNewDesignVATReturnUpdatedUIPageView);
 
             //Application.Current.MainPage.Navigation.PopAsync();
         }
