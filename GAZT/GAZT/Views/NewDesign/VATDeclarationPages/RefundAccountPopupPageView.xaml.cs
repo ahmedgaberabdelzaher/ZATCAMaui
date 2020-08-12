94454ce7629b1271046447cdacfee79536745cfc
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-12 17:48:30 +0530

    added Refund and New account Popup in VAT Return

---
+++ GAZT/GAZT/Views/NewDesign/VATDeclarationPages/RefundAccountPopupPageView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign.VATDeclarationPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RefundAccountPopupPageView : PopupPage
+    {
+        public RefundAccountPopupPageView()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PushAsync(new NewAccountPopPage());
+        }
+
+        private void SfButton_Clicked(object sender, EventArgs e)
+        {
+            Picker1.IsOpen = true;
+        }
+
+        private void SfButton_Clicked1(object sender, EventArgs e)
+        {
+            Picker2.IsOpen = true;
+        }
+    }
+}
