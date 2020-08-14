a2435c47b94d8e600d2de77a083ea7a063404b46
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-14 20:04:58 +0530

    Updated Taxpayer Profile Page

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -26,6 +26,7 @@ using EGAZT.Views.NewDesign.VATDeRegistration;
 using EGAZT.Views.NewDesign.ZakatForm5;
 using EGAZT.Views.NewDesign.TAXEvasionPages;
 using EGAZT.Views.NewDesign.VATRefunds;
+using EGAZT.Views.NewDesign.TaxpayerProfile;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace EGAZT
@@ -306,7 +307,8 @@ namespace EGAZT
             dialogService.Initialize(navigationPage);
             InitializeAppDynamics();
 
-            MainPage = navigationPage;
+            //MainPage = navigationPage;
+            MainPage = new TaxpayerProfilePageView();
         }
         public static void CreateClientHandler()
         {
