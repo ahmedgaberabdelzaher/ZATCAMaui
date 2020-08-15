0422adcca7286b9c77f7747cfc734a2698f57e69
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-15 15:56:42 +0530

    set default value to custom Label renderer

--- GAZT/GAZT/CustomControl/CustomLabel.cs
+++ GAZT/GAZT/CustomControl/CustomLabel.cs
@@ -4,7 +4,7 @@ namespace GAZT
 {
     public class CustomLabel : Label
     {
-        public double LineSpacing { get; set; }
+        public double LineSpacing { get; set; } = 0.5;
         public CustomLabel()
         {
         }
