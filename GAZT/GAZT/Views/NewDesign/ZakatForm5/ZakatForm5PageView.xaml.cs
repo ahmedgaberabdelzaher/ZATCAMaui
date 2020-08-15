7cfc5de68ef11dce13958d1ea4169fb11d1f096a
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-15 20:13:09 +0530

    Updated ZakatForm 5 financial Info and icons

--- GAZT/GAZT/Views/NewDesign/ZakatForm5/ZakatForm5PageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/ZakatForm5/ZakatForm5PageView.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
 using Xamarin.Forms.Xaml;
 
 namespace EGAZT.Views.NewDesign.ZakatForm5
@@ -23,10 +24,10 @@ namespace EGAZT.Views.NewDesign.ZakatForm5
             this.BindingContext = viewModel;
             //viewModel.z = true;
             //viewModel.IsNoDataLabelVisible = false;
-            IntialiseAsync();
+  //          IntialiseAsync();
         }
 
-        protected override void OnAppearing()
+/*        protected override void OnAppearing()
         {
             base.OnAppearing();
             App.IsComingFromSleepMode = false;
@@ -44,7 +45,7 @@ namespace EGAZT.Views.NewDesign.ZakatForm5
                     viewModel.IsLoading = false;
             });
         }
-
+        */
         private async Task LoadData()
         {
             try
@@ -100,6 +101,282 @@ namespace EGAZT.Views.NewDesign.ZakatForm5
         private void OnInfoTapped(object sender, EventArgs e)
         {
             PopupNavigation.Instance.PushAsync(new DummyPopUp());
+        }
+
+        private void Additional_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isAdditionalVisible = true;
+            Additionalbtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+ Cabsbtn.Style=SellBtn.Style=ProfessionalBtn.Style=LabourBtn.Style=IndustryBtn.Style=ContractingBtn.Style=Mineralsbtn.Style=InvestBtn.Style=Hotelsbtn.Style=EducationBtn.Style=Poultrybtn.Style=Carsbtn.Style= (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isMineralVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+
+        }
+
+        private void Minerals_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isMineralVisible = true;
+            Mineralsbtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = ContractingBtn.Style = Additionalbtn.Style = InvestBtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isAdditionalVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Cars_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isCarVisible = true;
+            Carsbtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = ContractingBtn.Style = Additionalbtn.Style = InvestBtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Poultry_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isPoultryVisible = true;
+            Poultrybtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = ContractingBtn.Style = Additionalbtn.Style = InvestBtn.Style = Hotelsbtn.Style = EducationBtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Education_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isEducationVisible = true;
+            EducationBtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = ContractingBtn.Style = Additionalbtn.Style = InvestBtn.Style = Hotelsbtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Hotels_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isHotelVisible = true;
+            Hotelsbtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = ContractingBtn.Style = Additionalbtn.Style = InvestBtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Invest_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isRealEstateVisible = true;
+            InvestBtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = ContractingBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Contracting_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isContractingVisible = true;
+            ContractingBtn.Style = (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = IndustryBtn.Style = InvestBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+
+            viewModel.isRealEstateVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Industry_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isIndustryVisible = true;
+            IndustryBtn.Style= (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = LabourBtn.Style = ContractingBtn.Style = InvestBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isContractingVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+
+        }
+
+        private void Labour_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isLabourOccupancyVisible = true;
+            LabourBtn.Style= (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = ProfessionalBtn.Style = IndustryBtn.Style = ContractingBtn.Style = InvestBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+
+            viewModel.isIndustryVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isBuyVisible = false ;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Sell_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isBuyVisible = true;
+            SellBtn.Style= (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = LabourBtn.Style = ProfessionalBtn.Style = IndustryBtn.Style = ContractingBtn.Style = InvestBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isProfessionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Professional_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isProfessionalVisible = true;
+            ProfessionalBtn.Style= (Style)Application.Current.Resources["SelectedBtn"];
+
+            Cabsbtn.Style = SellBtn.Style = SellBtn.Style = IndustryBtn.Style = ContractingBtn.Style = InvestBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isBuyVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
+            viewModel.isCabVisible = false;
+        }
+
+        private void Cabs_Clicked(object sender, EventArgs e)
+        {
+            viewModel.isCabVisible = true;
+            Cabsbtn.Style= (Style)Application.Current.Resources["SelectedBtn"];
+
+            ProfessionalBtn.Style = SellBtn.Style = SellBtn.Style = IndustryBtn.Style = ContractingBtn.Style = InvestBtn.Style = Additionalbtn.Style = Hotelsbtn.Style = EducationBtn.Style = Poultrybtn.Style = Mineralsbtn.Style = Carsbtn.Style = (Style)Application.Current.Resources["BackgroundWhiteBtn"];
+
+            viewModel.isProfessionalVisible = false;
+            viewModel.isBuyVisible = false;
+            viewModel.isLabourOccupancyVisible = false;
+            viewModel.isIndustryVisible = false;
+            viewModel.isContractingVisible = false;
+            viewModel.isRealEstateVisible = false;
+            viewModel.isHotelVisible = false;
+            viewModel.isEducationVisible = false;
+            viewModel.isPoultryVisible = false;
+            viewModel.isCarVisible = false;
+            viewModel.isMineralVisible = false;
+            viewModel.isAdditionalVisible = false;
         }
     }
 }
