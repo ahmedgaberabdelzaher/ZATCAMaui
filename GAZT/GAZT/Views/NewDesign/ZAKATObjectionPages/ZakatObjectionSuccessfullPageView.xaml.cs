9675efc8aa34428f35f97291f4b4d8b7ca59fee6
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-08-15 11:18:02 +0530

    ZAKAT return deatails invoice screens managed as per objection and without objection

--- GAZT/GAZT/Views/NewDesign/ZAKATObjectionPages/ZakatObjectionSuccessfullPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/ZAKATObjectionPages/ZakatObjectionSuccessfullPageView.xaml.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EGAZT.ViewModel.NewDesignViewModel.ZAKATObjectionPages;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace EGAZT.Views.NewDesign.ZAKATObjectionPages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ZakatObjectionSuccessfullPageView : ContentPage
-    {
+    { 
+        ZakatObjectionSuccessfullPageViewModel viewModel;
         public ZakatObjectionSuccessfullPageView()
         {
             InitializeComponent();
+        viewModel = App.Locator.ZakatObjectionSuccessfullPageView;
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+            this.BindingContext = viewModel;
+            Xamarin.Forms.NavigationPage.SetHasBackButton(this, false);
+
         }
     }
 }
