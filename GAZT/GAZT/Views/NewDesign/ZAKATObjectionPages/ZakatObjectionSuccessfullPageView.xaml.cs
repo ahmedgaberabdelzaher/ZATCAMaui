dda6c9824a0cbc87f750efccb7d813babec75808
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-08-15 14:46:27 +0530

    Invoice page designed and data binded properly

--- GAZT/GAZT/Views/NewDesign/ZAKATObjectionPages/ZakatObjectionSuccessfullPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/ZAKATObjectionPages/ZakatObjectionSuccessfullPageView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EGAZT.Models;
 using EGAZT.ViewModel.NewDesignViewModel.ZAKATObjectionPages;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
@@ -14,14 +15,35 @@ namespace EGAZT.Views.NewDesign.ZAKATObjectionPages
     public partial class ZakatObjectionSuccessfullPageView : ContentPage
     { 
         ZakatObjectionSuccessfullPageViewModel viewModel;
-        public ZakatObjectionSuccessfullPageView()
+        public ZakatObjectionSuccessfullPageView(ZakatReturnDetailsD ZakatReturnDetail)
         {
             InitializeComponent();
-        viewModel = App.Locator.ZakatObjectionSuccessfullPageView;
+            viewModel = App.Locator.ZakatObjectionSuccessfullPageView;
             On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
             this.BindingContext = viewModel;
             Xamarin.Forms.NavigationPage.SetHasBackButton(this, false);
+            SetLTR();
+            viewModel.OnPageLoad(ZakatReturnDetail);
+        }
 
+        private void SetLTR()
+        {
+            if (!App.IsArabic)
+            {
+                this.FlowDirection = FlowDirection.LeftToRight;
+            }
+        }
+
+        private void OnReturnClicked(object sender, EventArgs e)
+        {
+            viewModel._navigationService.GoBack();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Xamarin.Forms.Page pg = Navigation.NavigationStack[Navigation.NavigationStack.Count - 2];
+            Navigation.RemovePage(pg);
         }
     }
 }
