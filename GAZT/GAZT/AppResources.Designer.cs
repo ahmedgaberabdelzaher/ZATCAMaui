ace1bd42fa4e36e32f33543a70e5a0efc7e1b01a
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-08-23 13:54:19 +0530

    New strings added for Return Details

--- GAZT/GAZT/AppResources.Designer.cs
+++ GAZT/GAZT/AppResources.Designer.cs
@@ -9527,5 +9527,23 @@ namespace EGAZT {
                 return ResourceManager.GetString("ZZZZVATReturnsubmitted", resourceCulture);
             }
         }
+        
+        public static string NDInvoiceDetail {
+            get {
+                return ResourceManager.GetString("NDInvoiceDetail", resourceCulture);
+            }
+        }
+        
+        public static string NDSubmittedSuccessfully {
+            get {
+                return ResourceManager.GetString("NDSubmittedSuccessfully", resourceCulture);
+            }
+        }
+        
+        public static string NDObjectionSubmittedSuccessfully {
+            get {
+                return ResourceManager.GetString("NDObjectionSubmittedSuccessfully", resourceCulture);
+            }
+        }
     }
 }
