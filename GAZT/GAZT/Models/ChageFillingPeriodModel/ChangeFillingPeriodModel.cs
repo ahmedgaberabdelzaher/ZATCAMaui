bf1798d183f000f95b4cd0d09297748ca196476c
Mahesh <Rohith.kunchamwar@invenio-solutions.com>
2020-08-23 13:55:12 +0530

    Added missing files

---
+++ GAZT/GAZT/Models/ChageFillingPeriodModel/ChangeFillingPeriodModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EGAZT.Models.ChangeFillingPeriodModel
+{
+    public class ChangeFillingPeriodModel
+    {
+        public ChangeFillingPeriodModel()
+        {
+        }
+
+        public string ActiveOutletDecisionOptions { get; set; }
+        public bool ActiveOutletDecisionOptionsIsSelected { get; set; }
+    }
+
+
+    public class InstalmentAgreementAttachmentsModel
+    {
+        public InstalmentAgreementAttachmentsModel()
+        {
+        }
+
+        public string FieldTitle { get; set; }
+        public string FieldSubTitle { get; set; }
+        public string AttachmentName { get; set; }
+
+        public bool IsAttachmentAttached { get; set; }
+    }
+
+
+    public class MyRequestsListModel
+    {
+        public MyRequestsListModel()
+        {
+        }
+
+        public string Title { get; set; }
+        public string ReferenceNumber { get; set; }
+        public string Status { get; set; }
+        public string CurrentFrequency { get; set; }
+        public string NewFrequency { get; set; }
+        public string EffectiveDate { get; set; }
+        public string ReleaseDate { get; set; }
+    }
+
+
+}
