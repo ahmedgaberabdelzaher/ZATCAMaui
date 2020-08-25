234b1d806e0cecefde8c8b762e3818a15b6cbe03
Apple <apple@Apples-MacBook-Pro.local>
2020-08-25 10:26:58 +0530

    OnBoarding mirroring issue solved and tested on both language and platform

--- GAZT/GAZT.iOS/CustomRenderer/CustomLabelRenderer.cs
+++ GAZT/GAZT.iOS/CustomRenderer/CustomLabelRenderer.cs
@@ -50,6 +50,18 @@ namespace GAZT.iOS.CustomRenderer
                     }
                 }
 
+                if (e.NewElement != null)
+                {
+                    string StyleId = e.NewElement.StyleId;
+
+                    if (!string.IsNullOrEmpty(StyleId))
+                    {
+                        if (StyleId.Equals("OnBoardText"))
+                        {
+                            Control.TextAlignment = UITextAlignment.Center;
+                        }
+                    }
+                }
                 
                     //Line Spacing between two line in one Label
                     //if (!string.IsNullOrWhiteSpace(Element.Text))
@@ -63,124 +75,124 @@ namespace GAZT.iOS.CustomRenderer
                     //    var style = UIStringAttributeKey.ParagraphStyle;
                     //    var range = new NSRange(0, text.Length);
 
-                    //    text.AddAttribute(style, paragraphStyle, range);
+                //    text.AddAttribute(style, paragraphStyle, range);
 
-                    //    this.Control.AttributedText = text;
-                    //}
-                    //Line Spacing between two line in one Label
+                //    this.Control.AttributedText = text;
+                //}
+                //Line Spacing between two line in one Label
 
 
-                    //    Control.Font = UIFont.GetPreferredFontForTextStyle(new NSString("UICTFontTextStyleBody"));
-                    //    if (e.NewElement != null)
-                    //    {
-                    //        string StyleId = e.NewElement.StyleId;
-                    //        if (!string.IsNullOrEmpty(StyleId))
-                    //        {
-                    //            if (StyleId.Equals("Medium"))
-                    //            {
-                    //                var fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
-                    //                if (App.IsArabic)
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Regular", (float)fontSize);
-                    //                else
-                    //                    this.Control.Font = UIFont.FromName("Helvetica-Normal", (float)fontSize);
-                    //            }
-                    //            else if (StyleId.Equals("SmallBold"))
-                    //            {
-                    //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
-                    //                if (App.IsArabic)
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Regular", (float)fontSize);
-                    //                else
-                    //                    this.Control.Font = UIFont.FromName("Helvetica-Normal", (float)fontSize);
-                    //            }
-                    //            else if (StyleId.Equals("TwoLineText"))
-                    //            {
-                    //                this.Control.Lines = 2;
-                    //                this.Control.LineBreakMode = UILineBreakMode.TailTruncation;
-                    //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
-                    //                if (App.IsArabic)
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                else
-                    //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
-                    //            }
-                    //            else if (StyleId.Equals("FourLineText"))
-                    //            {
-                    //                if (Device.Idiom == TargetIdiom.Phone)
-                    //                {
-                    //                    if (App.IsArabic)
-                    //                        this.Control.Lines = 3;
-                    //                    else
-                    //                        this.Control.Lines = 6;
-                    //                }
-                    //                else
-                    //                {
-                    //                    if (App.IsArabic)
-                    //                        this.Control.Lines = 5;
-                    //                    else
-                    //                        this.Control.Lines = 8;
-                    //                }
-                    //                this.Control.LineBreakMode = UILineBreakMode.TailTruncation;
-                    //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
-                    //                if (App.IsArabic)
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                else
-                    //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
-                    //            }
-                    //            else if (StyleId.Equals("Micro"))
-                    //            {
-                    //                var fontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Label));
-                    //                if (App.IsArabic)
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                else
-                    //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
-                    //            }
-                    //            else if (StyleId.Equals("MicroTabBar"))
-                    //            {
-                    //                var fontSize = 12;
-                    //                if (App.IsArabic)
-                    //                {
-                    //                    fontSize = 10;
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                }
-                    //                else
-                    //                {
-                    //                    fontSize = 10;
-                    //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
-                    //                }
-                    //            }
-                    //            else if (StyleId.Equals("MicroTableText"))
-                    //            {
-                    //                var fontSize = 11;
-                    //                if (App.IsArabic)
-                    //                {
-                    //                    fontSize = 11;
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                }
-                    //            }
-                    //            else
-                    //            {
-                    //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
-                    //                if (App.IsArabic)
-                    //                {
-                    //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                    //this.Control.TextAlignment = UITextAlignment.Right;
-                    //                }
-                    //                else
-                    //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
-                    //            }
-                    //        }
-                    //        else
-                    //        {
-                    //            var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
-                    //            if (App.IsArabic)
-                    //            {
-                    //                this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
-                    //                //this.Control.TextAlignment = UITextAlignment.Right;
-                    //            }
-                    //            else
-                    //                this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
-                    //        }
-                    //    }
-                }
+                //    Control.Font = UIFont.GetPreferredFontForTextStyle(new NSString("UICTFontTextStyleBody"));
+                //    if (e.NewElement != null)
+                //    {
+                //        string StyleId = e.NewElement.StyleId;
+                //        if (!string.IsNullOrEmpty(StyleId))
+                //        {
+                //            if (StyleId.Equals("Medium"))
+                //            {
+                //                var fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+                //                if (App.IsArabic)
+                //                    this.Control.Font = UIFont.FromName("Cairo-Regular", (float)fontSize);
+                //                else
+                //                    this.Control.Font = UIFont.FromName("Helvetica-Normal", (float)fontSize);
+                //            }
+                //            else if (StyleId.Equals("SmallBold"))
+                //            {
+                //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+                //                if (App.IsArabic)
+                //                    this.Control.Font = UIFont.FromName("Cairo-Regular", (float)fontSize);
+                //                else
+                //                    this.Control.Font = UIFont.FromName("Helvetica-Normal", (float)fontSize);
+                //            }
+                //            else if (StyleId.Equals("TwoLineText"))
+                //            {
+                //                this.Control.Lines = 2;
+                //                this.Control.LineBreakMode = UILineBreakMode.TailTruncation;
+                //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+                //                if (App.IsArabic)
+                //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                else
+                //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
+                //            }
+                //            else if (StyleId.Equals("FourLineText"))
+                //            {
+                //                if (Device.Idiom == TargetIdiom.Phone)
+                //                {
+                //                    if (App.IsArabic)
+                //                        this.Control.Lines = 3;
+                //                    else
+                //                        this.Control.Lines = 6;
+                //                }
+                //                else
+                //                {
+                //                    if (App.IsArabic)
+                //                        this.Control.Lines = 5;
+                //                    else
+                //                        this.Control.Lines = 8;
+                //                }
+                //                this.Control.LineBreakMode = UILineBreakMode.TailTruncation;
+                //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+                //                if (App.IsArabic)
+                //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                else
+                //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
+                //            }
+                //            else if (StyleId.Equals("Micro"))
+                //            {
+                //                var fontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Label));
+                //                if (App.IsArabic)
+                //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                else
+                //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
+                //            }
+                //            else if (StyleId.Equals("MicroTabBar"))
+                //            {
+                //                var fontSize = 12;
+                //                if (App.IsArabic)
+                //                {
+                //                    fontSize = 10;
+                //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                }
+                //                else
+                //                {
+                //                    fontSize = 10;
+                //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
+                //                }
+                //            }
+                //            else if (StyleId.Equals("MicroTableText"))
+                //            {
+                //                var fontSize = 11;
+                //                if (App.IsArabic)
+                //                {
+                //                    fontSize = 11;
+                //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                }
+                //            }
+                //            else
+                //            {
+                //                var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+                //                if (App.IsArabic)
+                //                {
+                //                    this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                    //this.Control.TextAlignment = UITextAlignment.Right;
+                //                }
+                //                else
+                //                    this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
+                //            }
+                //        }
+                //        else
+                //        {
+                //            var fontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label));
+                //            if (App.IsArabic)
+                //            {
+                //                this.Control.Font = UIFont.FromName("Cairo-Light", (float)fontSize);
+                //                //this.Control.TextAlignment = UITextAlignment.Right;
+                //            }
+                //            else
+                //                this.Control.Font = UIFont.FromName("HelvLight", (float)fontSize);
+                //        }
+                //    }
+            }
         }
     }
 }
