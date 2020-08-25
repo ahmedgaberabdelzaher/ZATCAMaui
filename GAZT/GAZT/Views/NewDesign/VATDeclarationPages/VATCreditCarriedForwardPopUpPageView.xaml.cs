2b26f8a135f240c2c83cc51dac51ee7c9ae10fb9
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-25 17:48:42 +0530

    Added CreditCarriedForwardPopUpPageView in VATDeclaration

---
+++ GAZT/GAZT/Views/NewDesign/VATDeclarationPages/VATCreditCarriedForwardPopUpPageView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign.VATDeclarationPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VATCreditCarriedForwardPopUpPageView : PopupPage
+    {
+        public VATCreditCarriedForwardPopUpPageView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseTapped(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
