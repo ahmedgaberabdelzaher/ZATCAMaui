d37126fb99f9ec9409629ba89ae254d0e90bd322
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-25 10:42:32 +0530

    Added Anonymous Menu Page

---
+++ GAZT/GAZT/Views/NewDesign/Template/DashboardAnonymousMenuPageView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using EGAZT.ViewModel.NewDesignViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign.Template
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DashboardAnonymousMenuPageView : ContentPage
+    {
+        DashboardAnonymousMenuPageViewModel viewModel;
+        public DashboardAnonymousMenuPageView()
+        {
+            InitializeComponent();
+            viewModel = App.Locator.DashboardAnonymousMenuPageView;
+            BindingContext = viewModel;
+        }
+    }
+}
