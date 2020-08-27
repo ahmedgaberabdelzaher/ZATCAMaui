e594c3690e4ed55389fc501bbda67760f0c9cd76
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-27 11:49:49 +0530

    Added Support Page

---
+++ GAZT/GAZT/Views/NewDesign/SupportPages/SupportPageView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SupportPageView : ContentPage
+    {
+        public SupportPageView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBackArrowTapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
