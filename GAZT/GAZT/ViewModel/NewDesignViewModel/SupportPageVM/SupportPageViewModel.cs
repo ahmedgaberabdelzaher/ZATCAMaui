89f2a804fcc8cff96927ac3c9ba1a837fea2fada
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-08-27 13:25:55 +0530

    Integrated support Page to Menu Page after Login

--- GAZT/GAZT/ViewModel/NewDesignViewModel/SupportPageVM/SupportPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/SupportPageVM/SupportPageViewModel.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace EGAZT.ViewModel.NewDesignViewModel.SupportPageVM
+namespace EGAZT.ViewModel.NewDesignViewModel
 {
-    class SupportPageViewModel
+    public class SupportPageViewModel: BaseViewModel
     {
-    }
+        public readonly INavigationService _navigationService;
+        public readonly IDialogService _dialogService;
+
+        public SupportPageViewModel(INavigationService navigationService, IDialogService dialogService) : base(navigationService, dialogService)
+        {
+            if (navigationService == null)
+            {
+                throw new ArgumentNullException("navigationService");
+            }
+            _navigationService = navigationService;
+            if (dialogService == null)
+            {
+                throw new ArgumentNullException("dialogService");
+            }
+            _dialogService = dialogService;
+        }
+        }
 }
--- GAZT/GAZT/Views/NewDesign/SupportPages/SupportPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/SupportPages/SupportPageView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EGAZT.ViewModel.NewDesignViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,31 @@ namespace EGAZT.Views.NewDesign
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SupportPageView : ContentPage
     {
+        SupportPageViewModel viewModel;
         public SupportPageView()
         {
             InitializeComponent();
+            viewModel = App.Locator.SupportPageView;
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (App.IsArabic)
+            {
+                this.FlowDirection = FlowDirection.RightToLeft;
+                Resources["BackButtonArrow"] = Resources["ArrowImageForArabicStyle"];
+            }
+            else
+            {
+                this.FlowDirection = FlowDirection.LeftToRight;
+            }
         }
 
         private void OnBackArrowTapped(object sender, EventArgs e)
         {
-
+            viewModel._navigationService.GoBack();
         }
     }
 }
