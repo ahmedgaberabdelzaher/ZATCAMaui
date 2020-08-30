d2ee6185a275d8bed86c1343e55b705fc054e625
Sandeep Kumar <>
2020-08-31 01:49:01 +0530

    Strings added

--- GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
+++ GAZT/GAZT/ViewModel/SyncFusionEnabledViewModel/LoginPage/SFLoginPageViewModel.cs
@@ -993,9 +993,9 @@ namespace EGAZT.ViewModel.SyncFusionEnabledViewModel.SFLoginPage_ViewModel
 
                     Device.BeginInvokeOnMainThread(() =>
                     {
-                        _navigationService.NavigateTo(App.GAZTNewDesignDashBoardPageView);
+                        //_navigationService.NavigateTo(App.GAZTNewDesignDashBoardPageView);
                         //TODO for continue work on EST added by ashwini
-                        //_navigationService.NavigateTo(App.EstablishmentRegistrationPage);
+                        _navigationService.NavigateTo(App.EstablishmentRegistrationPage);
                     });
 
                     //if (App.IsOTPByPassed == true)
