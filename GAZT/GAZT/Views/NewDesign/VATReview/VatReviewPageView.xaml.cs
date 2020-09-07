cdbe00a5b7a9ee69b770b1cff0331faa96425d34
Mahesh <Rohith.kunchamwar@invenio-solutions.com>
2020-09-07 13:28:47 +0530

    Minor Bug fixes

---
+++ GAZT/GAZT/Views/NewDesign/VATReview/VatReviewPageView.xaml.cs
@@ -0,0 +1,121 @@
+﻿using EGAZT.Models;
+using EGAZT.Models.ZakatInstalationModels;
+using EGAZT.ViewModel.NewDesignViewModel.VatReviewViewModel;
+using EGAZT.Views.NewDesign.GenericPickers;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+using ItemTappedEventArgs = Syncfusion.ListView.XForms.ItemTappedEventArgs;
+
+namespace EGAZT.Views.NewDesign.VatReview
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VatReviewPageView : ContentPage
+    {
+        private VatReviewViewModel viewModel;
+        
+        public VatReviewPageView()
+        {
+            InitializeComponent();
+            Xamarin.Forms.NavigationPage.SetBackButtonTitle(this, "");
+            
+            ChangeAeroIcon();
+            SetLTR();
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+
+            viewModel = App.Locator.VatReviewView;
+            this.BindingContext = viewModel;
+            
+            viewModel.ResetData();
+
+            viewModel.VatReviewReasonDropDownData();
+        }
+        
+        private void SetLTR()
+        {
+            if (!App.IsArabic)
+            {
+                this.FlowDirection = FlowDirection.LeftToRight;
+            }
+        }
+        public void ChangeAeroIcon()
+        {
+            if (App.IsArabic)
+            {
+                Resources["StyleReverseBack"] = App.Current.Resources["ReverseBack"];
+            }
+            else
+            {
+                Resources["StyleReverseBack"] = App.Current.Resources["Back"];
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<PickerPageView, GenericPickerModel>(this, "PickerSelectedItem", (sender, arg) => {
+                //viewModel.PickerModel = arg;
+                //viewModel.updatePicker();
+                viewModel.updatePickerData(arg);
+            });
+            MessagingCenter.Subscribe<CalendarPickerPageView, GenericDatePickerModel>(this, "DatePickerSelectedItem",
+                (sender, arg) =>
+                {
+
+                    viewModel.PickedDate = arg.SelectedValue;
+                    viewModel.ValidateIdNumber();
+                });
+            
+            Xamarin.Forms.MessagingCenter.Subscribe<object, Attachments>(this, "AttachmentReceived", (sender, arg) =>
+            {
+                if (arg != null)
+                {
+                    viewModel.PopulateAttachments(arg.results);
+                }
+            });
+        }
+        
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            MessagingCenter.Unsubscribe<PickerPageView, GenericPickerModel>(this, "PickerSelectedItem");
+            MessagingCenter.Unsubscribe<CalendarPickerPageView, GenericDatePickerModel>(this, "DatePickerSelectedItem");
+            MessagingCenter.Unsubscribe<object, Attachments>(this, "AttachmentReceived");
+
+        }
+
+        private void Security_Type_ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var selectedITem = e.ItemData as VatReviewViewModel.SelectionModel;
+            if (selectedITem.SelectionTitle.Equals(AppResources.VRSADAD))
+            {
+                viewModel.EnableSadadSecurityView();
+            }else if (selectedITem.SelectionTitle.Equals(AppResources.VRBANKGURANTEE))
+            {
+                viewModel.EnablebankGuranteeSecurityView();
+            }
+        }
+
+        private void Report_Details_UnFocused(object sender, FocusEventArgs e)
+        {
+            viewModel.ReportDetails = Report_Details_Tx.Text;
+        }
+
+        private void ContactPersonTextUnFocus(object sender, FocusEventArgs e)
+        {
+            viewModel.ContactPersonName = ContactPersonEntry.Text;
+            viewModel.EnableDeclaration();
+        }
+
+        private void IdNumberTextChanged(object sender, TextChangedEventArgs e)
+        {
+            viewModel._idNumber = e.NewTextValue;
+        }
+
+        private void OnIDNumberFocusChanged(object sender, FocusEventArgs e)
+        {
+            viewModel.ValidateIdNumber();
+        }
+    }
+}
