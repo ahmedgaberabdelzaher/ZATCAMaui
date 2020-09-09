be7ee150fe6a9f2e71e0922543245bc9c084b2e1
ashwini <chandelashwini@hotmail.com>
2020-09-09 23:15:55 +0530

    activity item clear

--- GAZT/GAZT/Views/NewDesign/EstablishmentRegistrationPages/ActivityItemPage.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/EstablishmentRegistrationPages/ActivityItemPage.xaml.cs
@@ -26,6 +26,7 @@ namespace EGAZT.Views.NewDesign.EstablishmentRegistrationPages
             viewModel.validateCR = _activityNavigation.validateCR;
             //viewModel.cRActivityItem = _activityNavigation.cRActivityItem;
             viewModel.goBackAction = _activityNavigation.goBackAction;
+            viewModel.NregActivityList = _activityNavigation.taxPayerDetails?.Nreg_ActivitySet?.results.Where(i => i.Actno == $"{Int16.Parse(_activityNavigation.nextNumber?.Actno):000}").ToList();
             viewModel.CurrentTab = _activityNavigation.openedTab;
             BindingContext = viewModel;
             ChangeAeroIcon();
