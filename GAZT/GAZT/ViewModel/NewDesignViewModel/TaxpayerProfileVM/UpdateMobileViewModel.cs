ef3c9f842e0a7243c57f0e2b6c49a2270a0d7261
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-09-10 13:29:26 +0530

    Arabic text input issue solved on Verification page

--- GAZT/GAZT/ViewModel/NewDesignViewModel/TaxpayerProfileVM/UpdateMobileViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/TaxpayerProfileVM/UpdateMobileViewModel.cs
@@ -136,7 +136,7 @@ namespace EGAZT.ViewModel.NewDesignViewModel.TaxpayerProfileVM
                                 if (firstlettorOfNewMobileNumberEntryText.Equals("0"))
                                 {
                                     NewMobileNumberEntryText = string.Empty;
-                                    PopupNavigation.Instance.PushAsync(new AttachmentInformationPopUp("Mobile number can not start with 0"));
+                                   // PopupNavigation.Instance.PushAsync(new AttachmentInformationPopUp("Mobile number can not start with 0"));
 
                                 }
                             }
