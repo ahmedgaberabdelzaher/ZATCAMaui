beae6c37b5b433a108a0779891908fe7da9645e0
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-09-13 13:28:55 +0530

    Added Notes Poppages in VAT Declaration

---
+++ GAZT/GAZT/Views/NewDesign/VATDeclarationPages/NotesDescriptionPopUpPageView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign.VATDeclarationPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesDescriptionPopUpPageView : PopupPage
+    {
+        public NotesDescriptionPopUpPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GAZT/GAZT/Views/NewDesign/VATDeclarationPages/NotesPopUpPageView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign.VATDeclarationPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPopUpPageView : PopupPage
+    {
+        public NotesPopUpPageView()
+        {
+            InitializeComponent();
+            SetLTR();
+        }
+
+        private void SetLTR()
+        {
+
+            if (!App.IsArabic)
+            {
+                this.FlowDirection = FlowDirection.LeftToRight;
+            }
+            else
+            {
+                this.FlowDirection = FlowDirection.RightToLeft;
+            }
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            return true ;
+        }
+        protected override bool OnBackgroundClicked()
+        {
+            return true;
+        }
+        private void OnCloseTapped(object sender, EventArgs e)
+        {
+            try
+            {
+                PopupNavigation.Instance.PopAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void NoteDetail_Unfocused(object sender, FocusEventArgs e)
+        {
+
+        }
+    }
+}
