8a39f65258af93be09a3f120ba4aebdc6a0e5eae
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-09-13 18:13:57 +0530

    VAT Declaration attachment Page created

---
+++ GAZT/GAZT/Views/NewDesign/VATDeclarationPages/VATDeclarationAttachmentPageView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EGAZT.Views.NewDesign.VATDeclarationPages
+{
+    public partial class VATDeclarationAttachmentPageView : ContentPage
+    {
+        VATDeclarationAttachmentPageViewModel viewModel;
+        public VATDeclarationAttachmentPageView()
+        {
+            InitializeComponent();
+            viewModel = App.Locator.VATDeclarationAttachmentPageView;
+            this.BindingContext = viewModel;
+        }
+    }
+}
