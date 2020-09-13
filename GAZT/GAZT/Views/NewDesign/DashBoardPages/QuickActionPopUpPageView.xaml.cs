b64e8e7ad82b0062c2deed1be9c2b372bbd2614c
Ashish Ranjan <ashish.ranjan@invenio-solutions.com>
2020-09-13 15:35:13 +0530

    Image added in iOS and Android

--- GAZT/GAZT/Views/NewDesign/DashBoardPages/QuickActionPopUpPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/DashBoardPages/QuickActionPopUpPageView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using EGAZT.ViewModel.NewDesignViewModel;
+using GAZT.Models;
 using Rg.Plugins.Popup.Pages;
 using Rg.Plugins.Popup.Services;
 using Xamarin.Forms;
@@ -15,11 +16,43 @@ namespace EGAZT.Views.NewDesign.DashBoardPages
             InitializeComponent();
             viewModel = App.Locator.QuickActionPopUpPageView;
             this.BindingContext = viewModel;
+            SetLTR();
         }
 
+        private void SetLTR()
+        {
+            if (!App.IsArabic)
+            {
+                this.FlowDirection = FlowDirection.LeftToRight;
+            }
+        }
         private void OnCloseTapped(object sender, EventArgs e)
         {
             PopupNavigation.Instance.PopAsync();
+        }
+
+        private async void OnMyReturnsClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+            viewModel._navigationService.NavigateTo(App.GAZTNewDesignMyReturnsNewPageView, 4);
+
+        }
+
+        private async void OnMyBillsClicked(object sender, EventArgs e)
+        {
+            BillInfo billInfo = new BillInfo();
+            billInfo.BillTypeName = AppResources.All;
+            await PopupNavigation.Instance.PopAsync();
+            viewModel._navigationService.NavigateTo(App.GAZTNewDesignMyBillsPageView, billInfo);
+
+
+        }
+
+        private async void OnCorrespondanceClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+            viewModel._navigationService.NavigateTo(App.TaxpayerCorrespondancePageView);
+
         }
     }
 }
