544bc1aefac0ac11f918e56eef0d8bd93893792a
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-09-15 12:37:41 +0530

    17327 Done

--- GAZT/GAZT/Views/NewDesign/EstablishmentSignUP/SignUpForEstablishmentPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/EstablishmentSignUP/SignUpForEstablishmentPageView.xaml.cs
@@ -501,7 +501,9 @@ namespace EGAZT.Views.NewDesign.EstablishmentSignUP
                                 if (Result.d.NotFound == "X")
                                 {
                                     FrmCR.HasError = true;
-                                   // viewModel._dialogService.ShowMessage(AppResources.ZZPleaseentervalidCRnumber, AppResources.Information);
+                                    viewModel.IsAllValidCRNumberEntered = false;
+                                    viewModel.TxtCRNumber = string.Empty;
+                                    // viewModel._dialogService.ShowMessage(AppResources.ZZPleaseentervalidCRnumber, AppResources.Information);
                                     PopupNavigation.Instance.PushAsync(new AttachmentInformationPopUp(AppResources.ZZPleaseentervalidCRnumber));
                                 }
                                 else
@@ -535,6 +537,7 @@ namespace EGAZT.Views.NewDesign.EstablishmentSignUP
                    // PopupNavigation.Instance.PushAsync(new AddPopPageView(popUp));
                     PopupNavigation.Instance.PushAsync(new AttachmentInformationPopUp(AppResources.ZZCommercialReiterationNumbershouddbe10digits));
                     FrmCR.HasError = true;
+                    viewModel.IsAllValidCRNumberEntered = false;
                     EntryCRNumber.Text = string.Empty;
                    // EntryCRNumber.Focus();
                 }
@@ -2208,7 +2211,7 @@ namespace EGAZT.Views.NewDesign.EstablishmentSignUP
             //CRNumber Tile
             if (viewModel.IsCRChecked == true)
             {
-                if (viewModel.IsAllValidCRNumberEntered)
+                if (!string.IsNullOrEmpty(viewModel.TxtCRNumber))
                 {
                     viewModel.PageTitle = AppResources.ZZZContactInformation;
                     viewModel.BodyText = AppResources.ZZZZCompletethebelowdetails;
@@ -2216,6 +2219,7 @@ namespace EGAZT.Views.NewDesign.EstablishmentSignUP
                 }
                 else
                 {
+                    viewModel.TxtCRNumber = string.Empty;
                     FrmCR.HasError = true;
                    // viewModel._dialogService.ShowMessage(AppResources.ZZPleasefillthemandatoryfields, AppResources.Information);
                     PopupNavigation.Instance.PushAsync(new AttachmentInformationPopUp(AppResources.ZZPleasefillthemandatoryfields));
@@ -3970,9 +3974,10 @@ namespace EGAZT.Views.NewDesign.EstablishmentSignUP
 
         private void EntryCRNumber_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if(EntryCRNumber.Text.Length == 10)
+            if(!string.IsNullOrEmpty(EntryCRNumber.Text))
             {
-              //  EntryCRNumber.Unfocus();
+                FrmCR.HasError = false;
+                //  EntryCRNumber.Unfocus();
             }
         }
 
