6efbd308b22cd65c968b01e1775f8765e9f1d20f
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-09-21 20:03:02 +0530

    added WebView For GAZT and VAT Twitter  in contact us page

--- GAZT/GAZT/Views/NewDesign/SupportPages/SupportPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/SupportPages/SupportPageView.xaml.cs
@@ -123,14 +123,14 @@ namespace EGAZT.Views.NewDesign
         private void OnContactUsTapped(object sender, EventArgs e)
         {
             viewModel.setContactUs();
-            if (App.IsArabic)
-            {
-                //ContactUsWebView.Source = "https://gazt.gov.sa/ar/contactus/Pages/default.aspx";
-            }
-            else
-            {
-                //ContactUsWebView.Source = "https://gazt.gov.sa/en/contactus/Pages/default.aspx";
-            }
+            //if (App.IsArabic)
+            //{
+            //    //ContactUsWebView.Source = "https://gazt.gov.sa/ar/contactus/Pages/default.aspx";
+            //}
+            //else
+            //{
+            //    //ContactUsWebView.Source = "https://gazt.gov.sa/en/contactus/Pages/default.aspx";
+            //}
         }
 
         private void ChatWebView_Navigating(object sender, WebNavigatingEventArgs e)
@@ -166,5 +166,23 @@ namespace EGAZT.Views.NewDesign
         {
             viewModel._navigationService.NavigateTo(App.AboutUsPageView);
         }
+        private void OnTwitterGAZTTapped(object sender, EventArgs e)
+        {
+            viewModel.PageTitle = AppResources.NDGAZT;
+            ContactUsWebView.IsVisible = true;
+           
+                ContactUsWebView.Source = "https://twitter.com/GAZT_KSA";
+           
+        }
+        private void OnTwitterVATTapped(object sender, EventArgs e)
+        {
+            viewModel.PageTitle = AppResources.ZakatInstalmetSelectTypeVAT;
+            ContactUsWebView.IsVisible = true;
+            
+            ContactUsWebView.Source = "https://twitter.com/saudivat";
+
+        }
+        
+
     }
 }
