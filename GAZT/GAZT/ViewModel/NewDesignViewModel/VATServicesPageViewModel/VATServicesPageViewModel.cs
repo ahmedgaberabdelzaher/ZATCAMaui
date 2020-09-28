e82130b535d5014ca83826fbdec461f918eeda06
Parag Bihade <parag.bihade@invenio-solutions.com>
2020-09-28 08:14:02 +0530

    added VAT Services on new page

---
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/VATServicesPageViewModel/VATServicesPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EGAZT.ViewModel.NewDesignViewModel.VATServicesPageViewModel
+{
+    public class VATServicesPageViewModel : BaseViewModel
+    {
+        #region Constructor
+        public VATServicesPageViewModel(INavigationService navigationService, IDialogService dialogService) : base(navigationService, dialogService)
+        {
+
+        }
+        #endregion
+    }
+}
