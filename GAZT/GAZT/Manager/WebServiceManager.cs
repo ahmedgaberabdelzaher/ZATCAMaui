e246735faf5df8f75443a9ff790ec2bff08e8a0d
Sandeep Saini <Sandeep Saini@DESKTOP-PES2CFV>
2020-10-11 22:28:04 +0530

    Attachment not getting uploaded for Arabic Filenames in VATAmend

--- GAZT/GAZT/Manager/WebServiceManager.cs
+++ GAZT/GAZT/Manager/WebServiceManager.cs
@@ -2124,7 +2124,7 @@ namespace GAZT.Manager
 
                     client.DefaultRequestHeaders.Add("X-Requested-With", "X");
                     client.DefaultRequestHeaders.Add("Accept", "application/json");
-                    client.DefaultRequestHeaders.Add("slug", fileName);
+                    client.DefaultRequestHeaders.Add("slug", WebUtility.UrlEncode(fileName));
                     client.DefaultRequestHeaders.Add("ichannel", App.IncomingChannel);
 
                     ByteArrayContent baContent = new ByteArrayContent(AttachmentByte);
