798584b45c41ccc40e59ea8d2148367f72265101
Mahesh <Rohith.kunchamwar@invenio-solutions.com>
2020-10-15 18:45:43 +0530

    Zakat Instalment menu modifications.

--- GAZT/GAZT/ViewModel/NewDesignViewModel/InstalmentPlanViewModel/InstalmentPlanViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/InstalmentPlanViewModel/InstalmentPlanViewModel.cs
@@ -230,19 +230,19 @@ namespace EGAZT.ViewModel.NewDesignViewModel.InstalmentPlanViewModel
 
             var outletDecisionOptions = new ObservableCollection<InstalmentPlanModel>();
 
-            if(App.LoginDataRetrieved.ZkReg == "X") {
+            //if(App.LoginDataRetrieved.ZkReg == "X") {
 
-                outletDecisionOptions.Add(new InstalmentPlanModel
-                {
-                    ActiveOutletDecisionOptions = AppResources.ZakatInstalmetSelectTypeZakat,
-                    ActiveOutletDecisionOptionsIsSelected = false
-                });
-                outletDecisionOptions.Add(new InstalmentPlanModel
-                {
-                    ActiveOutletDecisionOptions = AppResources.ZakatInstalmetSelectTypeIncomeTax,
-                    ActiveOutletDecisionOptionsIsSelected = false
-                });
-            }
+            //    outletDecisionOptions.Add(new InstalmentPlanModel
+            //    {
+            //        ActiveOutletDecisionOptions = AppResources.ZakatInstalmetSelectTypeZakat,
+            //        ActiveOutletDecisionOptionsIsSelected = false
+            //    });
+            //    outletDecisionOptions.Add(new InstalmentPlanModel
+            //    {
+            //        ActiveOutletDecisionOptions = AppResources.ZakatInstalmetSelectTypeIncomeTax,
+            //        ActiveOutletDecisionOptionsIsSelected = false
+            //    });
+            //}
 
             if(App.LoginDataRetrieved.VtReg == "X") {
 
