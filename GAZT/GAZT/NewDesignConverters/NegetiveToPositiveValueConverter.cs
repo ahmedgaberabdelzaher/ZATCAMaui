13ec54c93eb4812fb9177f0b88a8d98a08da2de6
Sandeep Saini <Sandeep Saini@DESKTOP-PES2CFV>
2020-10-26 15:42:41 +0530

    Issue fix: Wrong Reassess amount displayed in VATReturn Request

---
+++ GAZT/GAZT/NewDesignConverters/NegetiveToPositiveValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EGAZT.NewDesignConverters
+{
+    public class NegetiveToPositiveValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.Parse(value.ToString()) < 0)
+            {
+                return Math.Abs(double.Parse(value.ToString()));
+            }
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
