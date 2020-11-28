4757c5153a569f56184162819e61edcd29fe0650
rohith.kunchamwar <rohith.kunchamwar@invenio-solutions.com>
2020-11-29 03:07:44 +0530

    Enabling App dynamics

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -749,11 +749,11 @@ namespace EGAZT
 
         public static void InitializeAppDynamics()
         {
-            //var config = AppDynamics.Agent.AgentConfiguration.Create("EUM-AAB-AUM");
-            //config.LoggingLevel = AppDynamics.Agent.LoggingLevel.Debug;
-            //AppDynamics.Agent.Instrumentation.enableAggregateExceptionReporting = true;
-            //config.CollectorURL = "https://eum.gazt.gov.sa:443";
-            //AppDynamics.Agent.Instrumentation.InitWithConfiguration(config);
+            var config = AppDynamics.Agent.AgentConfiguration.Create("EUM-AAB-AUM");
+            config.LoggingLevel = AppDynamics.Agent.LoggingLevel.Debug;
+            AppDynamics.Agent.Instrumentation.enableAggregateExceptionReporting = true;
+            config.CollectorURL = "https://eum.gazt.gov.sa:443";
+            AppDynamics.Agent.Instrumentation.InitWithConfiguration(config);
         }
 
         public static async void DisplayProgressView()
