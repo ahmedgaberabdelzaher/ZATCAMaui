d628610f3194ef80f0727791cc2637ca2d957658
rohith.kunchamwar <rohith.kunchamwar@invenio-solutions.com>
2020-11-30 04:06:29 +0530

    Removed icons

--- GAZT/GAZT/ViewModel/NewDesignViewModel/Common/GeneralServicesViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/Common/GeneralServicesViewModel.cs
@@ -86,14 +86,14 @@ namespace EGAZT.ViewModel.NewDesignViewModel.Common
             generalServicesListData.Add(new GeneralServicesListModel
                 {
                     ZDTitle = AppResources.NDVATRegistrationVerification,
-                    ZDImageSource = "requestVerification.png",
+                    ZDImageSource = "request_verification.png",
                     ArrowImageSource = fileImage
                 });
             
             generalServicesListData.Add(new GeneralServicesListModel
                 {
                     ZDTitle = AppResources.DBSMTaxEvasionReport,
-                    ZDImageSource = "taxEvasion_green.png",
+                    ZDImageSource = "tax_evasion_green.png",
                     ArrowImageSource = fileImage
                 });
                 GeneralServicesList = new ObservableCollection<GeneralServicesListModel>(generalServicesListData);
