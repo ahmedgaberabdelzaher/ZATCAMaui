cc1ce5cb50a59cdb0a24b55d5d05379315100507
Mehul Singhai <mehul.singhai@invenio-solutions.com>
2020-12-03 15:29:28 +0530

    Added code in My Entry Renderer and Custom Entry Renderer

--- GAZT/GAZT.iOS/CustomRenderer/CustomEntryRenderer.cs
+++ GAZT/GAZT.iOS/CustomRenderer/CustomEntryRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
 using GAZT;
 using GAZT.iOS.CustomRenderer;
 using UIKit;
@@ -22,6 +23,24 @@ namespace GAZT.iOS.CustomRenderer
             //else
             //this.Control.Font = UIFont.FromName("Helvetica-Normal", (float)fontSize);
             //this.Control.Font = UIFont.SystemFontOfSize((float)fontSize);
+           this.AddDoneButton();
+        }
+        protected void AddDoneButton()
+        {
+            var toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
+
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate
+            {
+                this.Control.ResignFirstResponder();
+                var baseEntry = this.Element.GetType();
+                ((IEntryController)Element).SendCompleted();
+            });
+
+            toolbar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+            };
+            this.Control.InputAccessoryView = toolbar;
         }
         //public CustomEntryRenderer()
         //{
--- GAZT/GAZT.iOS/CustomRenderer/MyEntryRenderer.cs
+++ GAZT/GAZT.iOS/CustomRenderer/MyEntryRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
 using GAZT.CustomControl;
 using GAZT.iOS.CustomRenderer;
 using UIKit;
@@ -31,6 +32,24 @@ namespace GAZT.iOS.CustomRenderer
 
             fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
             fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+            this.AddDoneButton();
+        }
+        protected void AddDoneButton()
+        {
+            var toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
+
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate
+            {
+                this.Control.ResignFirstResponder();
+                var baseEntry = this.Element.GetType();
+                ((IEntryController)Element).SendCompleted();
+            });
+
+            toolbar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+            };
+            this.Control.InputAccessoryView = toolbar;
         }
     }
 }
