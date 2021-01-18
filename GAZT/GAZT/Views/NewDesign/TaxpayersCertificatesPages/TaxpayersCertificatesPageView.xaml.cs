c4e66c685a9557c8fe485c734debf5f076131e13
divya.jannapureddy <divya.jannapureddy@invenio-solutions.com>
2021-01-18 21:41:50 +0530

    taxpayer certificates design changes

--- GAZT/GAZT/Views/NewDesign/TaxpayersCertificatesPages/TaxpayersCertificatesPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/TaxpayersCertificatesPages/TaxpayersCertificatesPageView.xaml.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
@@ -97,6 +98,11 @@ namespace EGAZT.Views.NewDesign.TaxpayersCertificatesPages
         protected override void OnAppearing()
         {
             base.OnAppearing();
+
+            var safeInsets = On<iOS>().SafeAreaInsets();
+            safeInsets.Bottom = -10;
+            this.Padding = safeInsets;
+
             if (Device.RuntimePlatform == Device.Android)
             {
                 TaxTypePicker.BackgroundColor = Color.FromHex("#f7f7f7");
