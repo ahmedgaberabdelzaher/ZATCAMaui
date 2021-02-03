d72f1af9e0704f862d6f275f7a260b0e47aa122c
divya.jannapureddy <divya.jannapureddy@invenio-solutions.com>
2021-02-03 17:21:03 +0530

    crash issue fixes in template

--- GAZT/GAZT/Views/NewDesign/Template/UnSelectedItemTemplate.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/Template/UnSelectedItemTemplate.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace EGAZT.Views.NewDesign.Template
 {
+    [Preserve(AllMembers = true)]
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UnSelectedItemTemplate : ViewCell
     {
