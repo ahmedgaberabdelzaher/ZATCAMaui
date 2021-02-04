8ab2f31cafb324cc47b228a2c1ff8bc3e12475be
MaheshBabu <Rohith.kunchamwar@invenio-solutions.com>
2021-02-04 12:26:32 +0530

    Build fixes

--- GAZT/GAZT/CustomControl/CheckboxCustom.xaml.cs
+++ GAZT/GAZT/CustomControl/CheckboxCustom.xaml.cs
@@ -6,10 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace GAZT.CustomControl
 {
+    [Preserve(AllMembers = true)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CheckboxCustom : ContentView
     {
--- GAZT/GAZT/Views/NewDesign/Template/SelectedItemTemplate.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/Template/SelectedItemTemplate.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace EGAZT.Views.NewDesign.Template
 {
+    [Preserve(AllMembers = true)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SelectedItemTemplate : ViewCell
     {
