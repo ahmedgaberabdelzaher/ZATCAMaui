a042b6a0cc3a281b04925a190167fc3c439b6e03
Satyanarayana <satyanarayana.m@invenio-solutions.com>
2021-02-10 18:45:35 +0530

    Payment Exception Page Added

---
+++ GAZT/GAZT/Views/NewDesign/PaymentOptions/PaymentExceptionPageView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace EGAZT.Views.NewDesign.PaymentOptions
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [Preserve(AllMembers = true)]
+    public partial class PaymentExceptionPageView : PopupPage
+    {
+        public PaymentExceptionPageView()
+        {
+            InitializeComponent();
+        }
+
+        async void TryAgainClicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
