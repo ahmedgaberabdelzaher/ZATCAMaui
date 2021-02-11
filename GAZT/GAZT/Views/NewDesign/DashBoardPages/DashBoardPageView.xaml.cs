8e12c1852b3f75b9e5342adfff93a4c9eb156633
Bigbrother <Bigbrother@DESKTOP-SJGJDMK>
2021-02-11 15:35:13 +0530

    Minor bug fixes, pointing to QA

--- GAZT/GAZT/Views/NewDesign/DashBoardPages/DashBoardPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/DashBoardPages/DashBoardPageView.xaml.cs
@@ -1233,7 +1233,7 @@ namespace EGAZT.Views.NewDesign.DashBoardPages
             {
                 await part.FadeTo(1, 600);
                 await btn_frameToolbar.FadeTo(0, 600);
-                await btn_frameToolbar.TranslateTo(xPosition - 10, -200, 200);
+                await btn_frameToolbar.TranslateTo(xPosition - 10, -200, 400);
 
                 //                  await btn_frameToolbar.TranslateTo(100, 0, 200, Easing.CubicInOut);
             }
@@ -1241,7 +1241,7 @@ namespace EGAZT.Views.NewDesign.DashBoardPages
             {
                 await part.FadeTo(0, 600);
                 await btn_frameToolbar.FadeTo(1, 600);
-                await btn_frameToolbar.TranslateTo(scrollView.X, scrollView.Y, 200);
+                await btn_frameToolbar.TranslateTo(scrollView.X, scrollView.Y, 400);
 
                 //await btn_frameToolbar.TranslateTo(0, 0, 200, Easing.CubicInOut);
             }
