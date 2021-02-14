eb8d009f2ce232630809e85a9f0b08ae55eab7f9
Satyanarayana <satyanarayana.m@invenio-solutions.com>
2021-02-14 22:08:48 +0530

    Account statements bug fixe

--- GAZT/GAZT/ViewModel/NewDesignViewModel/AccountStatements/AccountStatementsPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/AccountStatements/AccountStatementsPageViewModel.cs
@@ -1147,7 +1147,7 @@ namespace EGAZT.ViewModel.NewDesignViewModel.AccountStatements
             }
         }
 
-        public static string CalType = "G";
+        public string CalType = "G";
 
         public String FromTxAmount = "";
         public String ToTxAmount = "";
