c5fecbf7a6ce23b3966a230f4b34adf31368a55f
divya.jannapureddy <divya.jannapureddy@invenio-solutions.com>
2021-02-23 00:02:42 +0530

    20579 issue fix

--- GAZT/GAZT/ViewModel/NewDesignViewModel/VATDeRegistrationDetailsPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/VATDeRegistrationDetailsPageViewModel.cs
@@ -1284,6 +1284,8 @@ namespace EGAZT.ViewModel.NewDesignViewModel
 
         public async Task onPageLoad()
         {
+            string reqType = string.Empty;
+
             try
             {
                 VATDeregistrationModel = new VATDeregistrationModel();
@@ -1298,6 +1300,14 @@ namespace EGAZT.ViewModel.NewDesignViewModel
 
                 GetLastICRDate();
 
+                if (SelectedOutletOption.ActiveOutletDecisionOptions.Contains(AppResources.VATDeregistrationReasonType1))
+                {
+                    reqType = "VT_DREG";
+                }
+                else
+                {
+                    reqType = "VT_SUSP";
+                }
                 await Task.Run(() =>
                 {
                     IsLoading = true;
@@ -1312,6 +1322,19 @@ namespace EGAZT.ViewModel.NewDesignViewModel
                     {
                         vATDeRegistration = await VatRegistrationWebServiceManager.GAZTGetVATDeRegistrationData();
 
+                        if (reasonList == null)
+                        {
+                            reasonList = VATDeregistrationWebServiceManager.GAZTGETVATDeregReasonDropdownList(reqType);
+
+                        }
+                        for (int i = 0; i < reasonList.d.results.Count; i++)
+                        {
+                            if (reasonList.d.results[i].Reason == vATDeRegistration.d.Reason)
+                            {
+                                ReasonTitle = reasonList.d.results[i].Rdesc;
+
+                            }
+                        }
 
                         if (vATDeRegistration != null && vATDeRegistration.d != null)
                         {
@@ -1340,15 +1363,26 @@ namespace EGAZT.ViewModel.NewDesignViewModel
                             //Step 5
 
                             if (vATDeRegistration.d.Idnumbr != null)
-                                if (string.IsNullOrEmpty(IDType))
-                                {
-                                    TxtIDNumber = string.Empty;
+                            {
+                                TxtIDNumber = vATDeRegistration.d.Idnumbr;
 
-                                }
-                                else
-                                {
-                                    TxtIDNumber = vATDeRegistration.d.Idnumbr;
-                                }
+                            }
+                            if (vATDeRegistration.d.Type == "ZS00001")
+                            {
+                                IDType = AppResources.NationaID;
+
+                            }
+                            else if (vATDeRegistration.d.Type == "ZS00002")
+                            {
+                                IDType = AppResources.ZZIqamaID;
+
+                            }else
+                            {
+                                IDType = AppResources.ZZGCCID;
+
+                            }
+                           
+                               
                             ContactPersonName = vATDeRegistration.d.Contactnm;
                             ReturnIDx = vATDeRegistration.d.ReturnIdx;
 
@@ -1372,6 +1406,9 @@ namespace EGAZT.ViewModel.NewDesignViewModel
                             });
                         }
                         IsLoading = false;
+
+                        PopulateAttachments(vATDeRegistration.d.AttdetSet.results);
+
                     }
                     catch (GAZTVATRegistrationInProcessException ex)
                     {
@@ -1400,6 +1437,8 @@ namespace EGAZT.ViewModel.NewDesignViewModel
                 EnableReasonView();
                 IsDOBEditorVisible = false;
                 VoidIsVisible = false;
+
+
                 PopulateAttachmentsListViewTemplate();
             }
             catch (GAZTVATRegistrationInProcessException ex)
