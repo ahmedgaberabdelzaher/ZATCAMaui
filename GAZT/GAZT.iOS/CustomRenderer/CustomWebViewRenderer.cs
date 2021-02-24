b569826949ab1d9d8ac86caf6329888a1e0c0bfe
ashwini <chandelashwini@hotmail.com>
2021-02-24 19:53:14 +0530

    21106 hide Vertical Scroll Bar

---
+++ GAZT/GAZT.iOS/CustomRenderer/CustomWebViewRenderer.cs
@@ -0,0 +1,17 @@
+﻿using GAZT;
+using GAZT.iOS.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace GAZT.iOS.CustomRenderer
+{
+    public class CustomWebViewRenderer : WkWebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            (this.Subviews[0] as UIKit.UIScrollView).ShowsVerticalScrollIndicator = false;
+        }
+    }
+}
---
+++ GAZT/GAZT/CustomControl/CustomWebView.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms.Internals;
+
+namespace GAZT
+{
+    [Preserve(AllMembers = true)]
+    public class CustomWebView: Xamarin.Forms.WebView
+    {
+    }
+}
