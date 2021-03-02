4c847cacb9480b12d77685c048faac8e3915a294
Mahesh <Rohith.kunchamwar@invenio-solutions.com>
2021-03-02 12:57:45 +0530

    Dashboard Account statement fixes

--- GAZT/GAZT/Views/NewDesign/DashBoardPages/DashBoardPageView.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/DashBoardPages/DashBoardPageView.xaml.cs
@@ -54,13 +54,19 @@ namespace EGAZT.Views.NewDesign.DashBoardPages
                 viewModel.IfnotRegInVATAndZakat = false;
                 viewModel.IsBodyMyTaxVisible = false;
 
+                if(viewModel.AccountStatementsList != null) {
+
+                    viewModel.AccountStatementsList.Clear();
+                }
+
+                
                 //InstalmentsCollectionView.ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Horizontal)
                 //{
                 //    ItemSpacing = 10
                 //};
 
 
-               
+
 
                 SetLTR();
             }
