d69c734385ab107cfca5e08d2cf25e909598a021
divya.jannapureddy <divya.jannapureddy@invenio-solutions.com>
2021-03-21 12:09:41 +0530

    Build pointing to QA

--- GAZT/GAZT/Helper/Constants.cs
+++ GAZT/GAZT/Helper/Constants.cs
@@ -60,10 +60,10 @@ namespace GAZT.Helper
         //public static string DomainUrlForCookies = UatDomainForCookies;
         //public static string PartialDomainUrlForCookies = QAPrepprodProdPartialDomainForCookies;
 
-        public static string BaseUrlOfODataServices = PreProdBaseUrlForODataServices;
-        public static string BaseUrlOfAuthentication = PreProdBaseUrlForAuthentication;
-        public static string DomainUrlForCookies = PreprodDomainForCookies;
-        public static string PartialDomainUrlForCookies = QAPrepprodProdPartialDomainForCookies;
+        //public static string BaseUrlOfODataServices = PreProdBaseUrlForODataServices;
+        //public static string BaseUrlOfAuthentication = PreProdBaseUrlForAuthentication;
+        //public static string DomainUrlForCookies = PreprodDomainForCookies;
+        //public static string PartialDomainUrlForCookies = QAPrepprodProdPartialDomainForCookies;
 
         //public static string BaseUrlOfODataServices = ProdBaseUrlForODataServices;
         //public static string BaseUrlOfAuthentication = ProdBaseUrlForAuthentication;
