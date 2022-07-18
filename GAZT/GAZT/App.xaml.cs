bb2076a02a10342836fcd57115a80c293446f0d0
youxl <ahmedgaber20088@hotmail.com>
2022-07-18 10:39:24 +0300

    prod conf

--- GAZT/GAZT/App.xaml.cs
+++ GAZT/GAZT/App.xaml.cs
@@ -33,8 +33,8 @@ namespace EGAZT
     public partial class App : Application
     {
      // public static string CustomBaseUrl = "http://10.112.34.26:8024/";
-      public static string CustomBaseUrl = "https://stzgw-apic-gov.gazt.gov.sa/gazt-integration/test-third-party/v1/api/customs/";
-     // public static string CustomBaseUrl = "https://gw-apic-gov.gazt.gov.sa/gazt-integration/third-party/v1/api/customs/";
+      //public static string CustomBaseUrl = "https://stzgw-apic-gov.gazt.gov.sa/gazt-integration/test-third-party/v1/api/customs/";
+     public static string CustomBaseUrl = "https://gw-apic-gov.gazt.gov.sa/gazt-integration/third-party/v1/api/customs/";
 //public static string VatBaseUrl = "https://vatmobile.zatca.gov.sa/api";
       public static string VatBaseUrl = "http://172.50.15.39:8080/api";
   #region new design views
--- GAZT/GAZT/AppConfigurations/PageSettings.cs
+++ GAZT/GAZT/AppConfigurations/PageSettings.cs
@@ -4,7 +4,7 @@ namespace EGAZT.AppConfigurations
     public static class PageSettings
     {
         public static bool IsIncludeInquiryVisible= true;
-        public static bool IsIncludeTarrif = true;
+        public static bool IsIncludeTarrif = false;
         public static bool IsIncludeBalagh = false;
         public static bool IsIncludeLapFees = false;
         public static bool IsIncludeExicesTaxs = true;
@@ -12,7 +12,7 @@ namespace EGAZT.AppConfigurations
         public static string XZATCAClientSecretProd = "50393c1c3eda9bb35a6ea052c6954c79";
         public const string XZATCAClientIdTest = "a867a41eeccbd956b7f279b50d8535a5";
         public const string XZATCAClientSecretTest = "c9487460cd7dd8bc0f16ede707f4dad3";
-        public static Environment environment = Environment.Staging;
+        public static Environment environment = Environment.Prod;
         public  static string ExciseTaxUrl = "https://eservices.zatca.gov.sa/sites/sc/ar/app-view/Pages/Disclaimer.aspx";
         public static string GetClientID()
         {
