142b5a036c1e1ae4eb4746abbc8280b4ce2c337f
youxl <ahmedgaber20088@hotmail.com>
2022-08-15 13:26:26 +0300

    add Missing pages for EDecelrations

---
+++ GAZT/GAZT/Views/NewDesign/CustomServicesPages/e-Declarations/EDecleration.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using EGAZT.ViewModel.NewDesignViewModel.CustomServicesViewModels;
+using Xamarin.Forms;
+
+namespace EGAZT.Views.NewDesign.CustomServicesPages.eDeclarations
+{
+    public partial class EDeclerationView : ContentPage
+    {
+        E_DeclerationViewModel viewModel;
+        public EDeclerationView()
+        {
+            viewModel = App.Locator.eDeclerationViewModel;
+            BindingContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
