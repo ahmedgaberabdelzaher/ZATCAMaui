270be32d3155b5bb45c513fb851bd7e40fbda0bc
youxl <ahmedgaber20088@hotmail.com>
2022-08-17 14:28:10 +0300

    Home ViewModel

---
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/HomeViewModels/HomeViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Views;
+using Xamarin.Forms;
+
+namespace EGAZT.ViewModel.NewDesignViewModel.HomeViewModels
+{
+    public enum Tabs
+    {
+        Home,
+        Account,
+        Menu
+    }
+
+    public class HomeViewModel:BaseViewModel
+    {
+
+        string title ;
+        public string Title { get { return title; } set { title = value; RaisePropertyChanged(); } }
+
+        int currentTab=1;
+        public int CurrentTab { get { return currentTab; } set { currentTab = value; RaisePropertyChanged(); }  }
+        public HomeViewModel(INavigationService navigationServices, IDialogService dialogService) : base(navigationServices, dialogService)
+        {
+
+        }
+        public ICommand ChangeCurrentTabCommand
+        {
+            get
+            {
+                return new Command<string>((tab) =>
+                {
+                    if (tab!=currentTab.ToString())
+                    {
+                        if (tab=="1")
+                        {
+                           
+                            _navigationService.NavigateTo($"../NavigationPage/{App.SFLoginPageView}", App.GAZTNewDesignDashBoardPageView);
+                          
+                        }
+                        CurrentTab = int.Parse(tab);
+                        Title = tab == "0" ? AppResources.Home : AppResources.ZZZMenu;
+                    }
+              
+                });
+            }
+        }
+
+    }
+}
