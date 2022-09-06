426672690d793af7400294b1354d7e9188b74f41
Ahmed Hesham <ataha@youxel.com>
2022-09-06 14:52:35 +0300

    Adjust Report type to get data from api

---
+++ GAZT/GAZT/Models/SubmitReportModel/ReportTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EGAZT.Models.SubmitReportModel
+{
+    public class ReportTypeModel
+    {
+        public string reportTaxTypeId { get; set; }
+        public string reportTaxTypeCode { get; set; }
+        public string reportTaxTypeName { get; set; }
+    }
+    public class ReportTypeList
+    {
+        public List<ReportTypeModel> reportTaxTypeList { get; set; }
+    }
+}
+
