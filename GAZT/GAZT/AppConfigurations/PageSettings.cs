205d4b7768c3d992bd91ae5a8c123715086dc4e4
Ahmed Hesham <ataha@youxel.com>
2022-09-20 14:23:58 +0300

    Add App Center environment

--- GAZT/GAZT/AppConfigurations/PageSettings.cs
+++ GAZT/GAZT/AppConfigurations/PageSettings.cs
@@ -27,17 +27,17 @@ namespace EGAZT.AppConfigurations
         public static string XZATCAClientSecretProd = "50393c1c3eda9bb35a6ea052c6954c79";
         public const string XZATCAClientIdTest = "a867a41eeccbd956b7f279b50d8535a5";
         public const string XZATCAClientSecretTest = "c9487460cd7dd8bc0f16ede707f4dad3";
-        public static Environment environment = Environment.Prod;
+        public static string Target_Environment = "";
         public  static string ExciseTaxUrl = "https://eservices.zatca.gov.sa/sites/sc/ar/app-view/Pages/Disclaimer.aspx";
         public static string GetClientID()
         {
             string key = "";
-            switch (environment)
+            switch (Target_Environment)
             {
-                case Environment.Staging:
+                case"STG":
                     key= XZATCAClientIdTest;
                     break;
-                case Environment.Prod:
+                case "Prod":
                     key= XZATCAClientIdProd;
                     break;
                 default:
@@ -48,12 +48,12 @@ namespace EGAZT.AppConfigurations
         public static string GetClientSecret()
         {
             string key = "";
-            switch (environment)
+            switch (Target_Environment)
             {
-                case Environment.Staging:
+                case "STG":
                     key = XZATCAClientSecretTest;
                     break;
-                case Environment.Prod:
+                case "Prod":
                     key = XZATCAClientSecretProd;
                     break;
                 default:
