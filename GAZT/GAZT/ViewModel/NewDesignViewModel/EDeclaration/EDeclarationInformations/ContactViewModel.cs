11599f298f843b8924bc44c91b185b67352a1bc0
Ahmed Hesham <ataha@youxel.com>
2022-12-11 23:32:24 +0300

    Add dummy data

--- GAZT/GAZT/ViewModel/NewDesignViewModel/EDeclaration/EDeclarationInformations/ContactViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/EDeclaration/EDeclarationInformations/ContactViewModel.cs
@@ -132,6 +132,7 @@ namespace EGAZT.ViewModel.NewDesignViewModel.EDeclaration.EDeclarationInformatio
 
             }
             SubmitModel.travelerDeclaration.phoneNumber = "+966" + SubmitModel.travelerDeclaration.phoneNumber;
+            SubmitModel.travelerDeclaration.tripeType = 1;
             return true;
 
         }
