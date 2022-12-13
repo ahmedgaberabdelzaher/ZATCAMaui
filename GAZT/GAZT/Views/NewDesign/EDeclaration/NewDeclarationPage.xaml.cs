737ea5db3c707480fd08922653b54fff6a0b3ec8
youxl <ahmedgaber20088@hotmail.com>
2022-12-13 11:01:52 +0300

    ResetTravellers Data When Open Page

--- GAZT/GAZT/Views/NewDesign/EDeclaration/NewDeclarationPage.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/EDeclaration/NewDeclarationPage.xaml.cs
@@ -12,14 +12,15 @@ namespace EGAZT.Views.NewDesign.EDeclaration
         {
             InitializeComponent();
             viewModel = App.Locator.BaseEDeclarationViewModel;
+            viewModel.SubmitModel.travelerDeclaration = new Models.EDeclerationsModel.SubmitModels.TravelerDeclaration();
            // viewModel.SubmitModel.travelerDeclaration.Isvisitor = false;
-            //viewModel.SubmitModel.travelerDeclaration.travelingType = 1;
             BindingContext = viewModel;
         }
         public NewDeclarationPage(IDictionary<string, object> payload)
         {
             InitializeComponent();
             viewModel = App.Locator.BaseEDeclarationViewModel;
+            viewModel.SubmitModel.travelerDeclaration = new Models.EDeclerationsModel.SubmitModels.TravelerDeclaration();
             viewModel.SubmitModel.travelerDeclaration.Isvisitor = false;
             BindingContext = viewModel;
             viewModel.IamLoginPayloadData = payload;
