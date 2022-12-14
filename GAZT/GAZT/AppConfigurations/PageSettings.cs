13347f3774f863a68c84406ee1c18439e6072601
youxl <ahmedgaber20088@hotmail.com>
2022-12-14 10:58:44 +0300

    Pass SubmitResultTo Payment Page

--- GAZT/GAZT/AppConfigurations/PageSettings.cs
+++ GAZT/GAZT/AppConfigurations/PageSettings.cs
@@ -39,8 +39,8 @@ namespace EGAZT.AppConfigurations
 
         public static string DATAPowerSTGZATCABaseUrl = "https://stzgw-apic-gov.gazt.gov.sa/gazt-integration/test-third-party/";
         public static string DATAPowerProdCZATCABaseUrl ="https://stzgw-apic-gov.gazt.gov.sa/gazt-integration/test-third-party/";
-        public static string IAMLoginSTGBaseUrl = "http://pre-eservices.zatca.gov.sa/Iam/_iam/Iaminit.aspx?APPID=Mobile";
-        //public static string IAMLoginSTGBaseUrl = "http://pre-eservices.zatca.gov.sa/Iam/_iam/Iaminit.aspx?APPID=Mobile";
+        public static string IAMLoginSTGBaseUrl = "https://pre-eservices.zatca.gov.sa/Iam/_iam/Iaminit.aspx?APPID=Mobile";
+        //public static string IAMLoginSTGBaseUrl = "https://pre-eservices.zatca.gov.sa/Iam/_iam/Iaminit.aspx?APPID=Mobile";
 
         public static string CustomBaseUrl;
         public static string IAMLoginBaseUrl;
