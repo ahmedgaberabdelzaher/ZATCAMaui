c2c27af463422156a6e271d3a73d96aed86701e3
youxl <ahmedgaber20088@hotmail.com>
2022-12-18 17:34:49 +0300

    paymentURl

--- GAZT/GAZT/AppConfigurations/PageSettings.cs
+++ GAZT/GAZT/AppConfigurations/PageSettings.cs
@@ -41,7 +41,7 @@ namespace EGAZT.AppConfigurations
         public static string DATAPowerProdCZATCABaseUrl ="https://stzgw-apic-gov.gazt.gov.sa/gazt-integration/test-third-party/";
         public static string IAMLoginSTGBaseUrl = "https://peservices.zatca.gov.sa/Iamext/_iam/Iaminit.aspx?APPID=Mobile";
         //public static string IAMLoginSTGBaseUrl = "https://pre-eservices.zatca.gov.sa/Iam/_iam/Iaminit.aspx?APPID=Mobile";
-        public static string CustomPaymentSTGURl = "https://peservices.zatca.gov.sa:4433/payment/initiate/";
+        public static string CustomPaymentSTGURl = "https://payments-peservices.zatca.gov.sa/payment/initiate";
 
         public static string CustomBaseUrl;
         public static string IAMLoginBaseUrl;
