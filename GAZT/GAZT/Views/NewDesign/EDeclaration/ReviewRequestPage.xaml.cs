410915f6b6100d9e483052d88203f6ee0a917a47
Ahmed Hesham <ataha@youxel.com>
2023-01-17 15:04:03 +0300

    Add details fees in review E-Declaration

--- GAZT/GAZT/Views/NewDesign/EDeclaration/ReviewRequestPage.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/EDeclaration/ReviewRequestPage.xaml.cs
@@ -23,6 +23,7 @@ namespace EGAZT.Views.NewDesign.EDeclaration
             App.Locator.StateManager.DeleteItem("inquireDeclaration");
             viewModel.Inquire = new TravelerDeclarationResponse();
             viewModel.InquireList = new ObservableCollection<BottomSheetModel>();
+            viewModel.DetailsTotalFeesList = new ObservableCollection<BottomSheetModel>();
             base.OnDisappearing();
         }
     }
