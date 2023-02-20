e58eed22210b8282e0a1c85e36edc91c6583b8a4
Ahmed Hesham <ataha@youxel.com>
2023-02-20 12:31:40 +0300

    Adjust Separator in bottom sheet

---
+++ GAZT/GAZT/Converters/RemoveLastLineConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using Xamarin.Forms;
+using EGAZT.Controls;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EGAZT.Converters
+{
+	public class RemoveLastLineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (parameter != null && parameter is CollectionView)
+                {
+                    var collectionView = ((CollectionView)parameter);
+                    var bottomSheetList = collectionView?.ItemsSource as ObservableCollection<BottomSheetModel>;
+                    var test = collectionView?.ItemsSource as List<BottomSheetModel>;
+
+                    if (bottomSheetList != null)
+                    {
+                        foreach (var item in bottomSheetList)
+                        {
+                            if (bottomSheetList.Last() == item)
+                                item.HasLine = false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+           
+              
+                   
+            var eventArgs = value as SelectedItemChangedEventArgs;
+
+            return eventArgs.SelectedItem;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
