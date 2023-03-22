dc7d9db5c6abb7273a6019d25e98a634ddb029f0
Ahmed Gaber <ahmedgaber20088@hotmail.com>
2023-03-22 10:34:26 +0300

    EdeclerationProdUrl

--- GAZT/GAZT/Views/NewDesign/CustomServicesPages/e-Declarations/CreateE_Declaration.xaml.cs
+++ GAZT/GAZT/Views/NewDesign/CustomServicesPages/e-Declarations/CreateE_Declaration.xaml.cs
@@ -18,7 +18,7 @@ namespace EGAZT.Views.NewDesign.CustomServicesPages.eDeclarations
             header.TitleText = title;
             if (title == AppResources.eDeclaration)
             {
-                wbview.Source = "https://pre-eservices.zatca.gov.sa/sites/sc/ar/pages/Home.aspx";
+                wbview.Source = PageSettings.GetNewEDeclarationLinks();
             }
             else if (title == AppResources.Transactiondescription)
             {
