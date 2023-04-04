8c24320f52b508407056377df21bbd5e2684ffa9
Ahmed Gaber <ahmedgaber20088@hotmail.com>
2023-04-04 13:31:08 +0300

    change fasahPostmethod

--- GAZT/GAZT/Services/Classes/UserServices.cs
+++ GAZT/GAZT/Services/Classes/UserServices.cs
@@ -18,17 +18,17 @@ namespace EGAZT.Services.Classes
         }
         public async Task<HttpResponseMessage> FasahLogin(FasahLoginBody body)
         {
-            var response = await HttpManager.PostAsync(PageSettings.FasahBaseUrl+ "api/login", body, false,"",false,true).ConfigureAwait(false);
+            var response = await HttpManager.FasahPostAsync(PageSettings.FasahBaseUrl+ "api/login", body, false,"",false,true).ConfigureAwait(false);
             return response;
         }
         public async Task<HttpResponseMessage> VerifyFasahLoginOtp(VerifyLoginBody body,string fasahLoginHeader)
         {
-            var response = await HttpManager.PostAsync(PageSettings.FasahBaseUrl+ "api/user/login/verify", body, false, fasahLoginHeader, false, true).ConfigureAwait(false);
+            var response = await HttpManager.FasahPostAsync(PageSettings.FasahBaseUrl+ "api/user/login/verify", body, false, fasahLoginHeader, false, true).ConfigureAwait(false);
             return response;
         }
         public async Task<HttpResponseMessage> ResendOtp(ResendOtpBody body, string fasahLoginHeader)
         {
-            var response = await HttpManager.PostAsync(PageSettings.FasahBaseUrl + "api/user/login/resendToken", body, false, fasahLoginHeader, false, true).ConfigureAwait(false);
+            var response = await HttpManager.FasahPostAsync(PageSettings.FasahBaseUrl + "api/user/login/resendToken", body, false, fasahLoginHeader, false, true).ConfigureAwait(false);
             return response;
         }
     }
