b4258f211d4992014822b251ff825bc3d312ab3c
Ahmed Gaber <ahmedgaber20088@hotmail.com>
2023-07-06 15:50:20 +0300

    inssurance fees

--- GAZT/GAZT/ViewModel/NewDesignViewModel/HomeViewModels/HomeViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/HomeViewModels/HomeViewModel.cs
@@ -260,11 +260,7 @@ namespace EGAZT.ViewModel.NewDesignViewModel.HomeViewModels
                 {
                    Name=AppResources.TransactionReception, ID="IAMLoginView",ImageSource="TransactionReceptionIcon",ColumnNo=0,Row=isvertical?4:2,IsVerticalView=isvertical,ServiceDesc=isvertical?AppResources.TawreedServiceDesc:""
                 }
-                 /*,
-                 new MenuModel()
-                {
-                   Name=AppResources.InquireaboutPaymentofInsuranceTitle, ID="LaboratoryPaymentOfInsuranceFees",ImageSource="LabfeesInquiry",ColumnNo=0,Row=isvertical?2:1,IsVerticalView=isvertical,ServiceDesc=isvertical?AppResources.CustomNote4:""
-                }*/
+               
                  ,
                   new MenuModel()
                 {
@@ -276,6 +272,11 @@ namespace EGAZT.ViewModel.NewDesignViewModel.HomeViewModels
                 {
                      
                    Name=AppResources.CarImport, ID="FasahLoginView",ImageSource="BrokerOptionality",ColumnNo=0,Row=isvertical?6:3,IsVerticalView=isvertical,ServiceDesc=isvertical?"":""
+                }
+                    ,
+                 new MenuModel()
+                {
+                   Name=AppResources.InquireaboutPaymentofInsuranceTitle, ID="LaboratoryPaymentOfInsuranceFees",ImageSource="LabfeesInquiry",ColumnNo=isvertical?0:1,Row=isvertical?7:3,IsVerticalView=isvertical,ServiceDesc=isvertical?AppResources.CustomNote4:""
                 }
                 
            };
