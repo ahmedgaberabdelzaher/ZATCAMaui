275e3d254b38dda84a94c92bd50c8b9517963e28
Ahmed Gaber <ahmedgaber20088@hotmail.com>
2023-10-16 20:32:11 +0300

    Qr Fixes

--- GAZT/GAZT/ViewModel/NewDesignViewModel/TahqaqViewModels/TahqaqScanPageViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/TahqaqViewModels/TahqaqScanPageViewModel.cs
@@ -264,6 +264,35 @@ namespace EGAZT.ViewModel.NewDesignViewModel.TahqaqViewModels
                                 }
                             }
                             var res = qrValidation(eInvoiceQRModel);
+                            if ((NoofTags <= 5 && res != "") || (NoofTags <= 8 && res != ""))
+                            {
+                                currentPosition = 0;
+                                tagNumber = 0;
+                                while (currentPosition < byteList.Length)
+                                {
+                                    // int tagNumber = byteList[currentPosition];
+
+                                    currentPosition++;
+                                    tagNumber++;
+                                    // Read Length
+                                    int valueLength = byteList[currentPosition];
+                                    Debug.WriteLine(valueLength);
+
+                                    currentPosition++;
+                                    // Read Message
+                                    int lastPosition = currentPosition + valueLength + 1;
+                                    var message = byteList.Skip(currentPosition).Take(lastPosition - (currentPosition + 1));
+                                    String messageAsText = Encoding.UTF8.GetString(message.ToArray());
+                                    Debug.WriteLine(messageAsText);
+                                    // Utf8Decoder().convert(message.toList());
+                                    currentPosition += valueLength;
+                                    SetDataToModel(tagNumber, messageAsText);
+                                    NoofTags = tagNumber;
+
+
+                                }
+                                res = qrValidation(eInvoiceQRModel);
+                            }
                             if (res == "")
                             {
                                 bool isIntegrated = NoofTags == 9 || NoofTags == 8 ? true : false;
@@ -662,7 +691,7 @@ namespace EGAZT.ViewModel.NewDesignViewModel.TahqaqViewModels
                             {
                                 RegistredStatusWithDisplaQRRslt();
                             }
-                            if (NoofTags == 5 && EInvEnfStatus == 1)
+                            else if (NoofTags == 5 && EInvEnfStatus == 1)
                             {
                                 IsShowMsgView = true;
                                 MessageTxt = AppResources.InvalidQrMessage;
@@ -678,14 +707,17 @@ namespace EGAZT.ViewModel.NewDesignViewModel.TahqaqViewModels
                                 IsClearedStatusVisible = true;
 
                             }
-                            if (NoofTags == 8)
-                            {
-                                RegistredStatusWithDisplaQRRslt();
-                                // IsShowSubmitReport = false;
-                            }
                             else
                             {
-                                RegistredStatusWithDisplaQRRslt();
+                                if (NoofTags == 8)
+                                {
+                                    RegistredStatusWithDisplaQRRslt();
+                                    // IsShowSubmitReport = false;
+                                }
+                                else
+                                {
+                                    RegistredStatusWithDisplaQRRslt();
+                                }
                             }
                         }
                         else
