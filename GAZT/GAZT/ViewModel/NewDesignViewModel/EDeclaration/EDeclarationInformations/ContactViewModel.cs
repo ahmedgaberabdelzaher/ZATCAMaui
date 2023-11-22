b23da4144fdd094e0abe7d3aac587294a180d5c1
Ahmed Hesham <>
2023-11-22 15:50:13 +0300

    Adjust Trip card cherry pick in dev

--- GAZT/GAZT/ViewModel/NewDesignViewModel/EDeclaration/EDeclarationInformations/ContactViewModel.cs
+++ GAZT/GAZT/ViewModel/NewDesignViewModel/EDeclaration/EDeclarationInformations/ContactViewModel.cs
@@ -190,6 +190,15 @@ namespace EGAZT.ViewModel.NewDesignViewModel.EDeclaration.EDeclarationInformatio
                                 TravelerDeclarationResponse.fees?.ForEach(f => { DetailsTotalFeesList.Add(new BottomSheetModel { Name = f.Name, Id = (Math.Round(f.value, 2)).ToString() }); });
 
                                 _navigationService.NavigateTo("/EDeclarationSuccessPage");
+
+                                TripCard.AirImage = "QSelected.png";
+                                TripCard.SeaImage = "QUnselected.png";
+                                TripCard.LandImage = "QUnselected.png";
+
+                                TripCard.AirTextColor = Color.White;
+                                TripCard.SeaTextColor = Color.FromHex("#002447");
+                                TripCard.LandTextColor = Color.FromHex("#002447");
+
                                 MobileNumber = string.Empty;
                             }
                             
