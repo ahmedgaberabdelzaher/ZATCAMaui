3f12f86c468821329058fd9e35df8965400511f2
rshaikh-c <rshaikh-c@zatca.gov.sa>
2024-10-14 13:35:10 +0530

    Maui Display date format corrected

---
+++ ZATCAMAUI/Core/Converters/DateFormatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ZATCAMAUI.Core.Converters
+{
+    public class DateFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string dateString && DateTime.TryParse(dateString, out DateTime dateTime))
+            {
+                // Format the DateTime to yyyy-MM-dd
+                return dateTime.ToString("yyyy/MM/dd");
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
