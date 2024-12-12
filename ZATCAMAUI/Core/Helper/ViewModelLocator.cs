8569f38a8ea5e782157ae8eb61f4c2126d71e4a9
rshaikh-c <rshaikh-c@zatca.gov.sa>
2024-12-12 19:26:47 +0530

    ZakatExemption Attachments issue fixes.

--- ZATCAMAUI/Core/Helper/ViewModelLocator.cs
+++ ZATCAMAUI/Core/Helper/ViewModelLocator.cs
@@ -324,7 +324,7 @@ namespace ZATCAMAUI.Core.Helper
                 .AddSingleton<VATInstalmentNotesPageViewModel>()
                 .AddSingleton<VATInstalmentPlanRevokeViewModel>()
                 .AddSingleton<OTPPageViewModel>()
-                .AddSingleton<AttachmentViewModel>()
+                .AddTransient<AttachmentViewModel>()
                 .AddSingleton<AccountLockedViewModel>()
 
             #endregion
