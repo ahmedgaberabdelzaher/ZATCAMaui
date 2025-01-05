191f6eee2197b2074b8b53091df94aee2037bc6c
Ahmed Hesham <>
2025-01-05 23:32:29 +0300

    Self signed certificate

---
+++ ZATCAMAUI/Platforms/iOS/Helper/WebViewDelegate.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using WebKit; 
+namespace ZATCAMAUI.Platforms.iOS.Helper
+{
+    // Self signed Certificate 
+    public class WebViewDelegate : WKNavigationDelegate, INSUrlConnectionDataDelegate
+    {
+        public override void DidReceiveAuthenticationChallenge(WKWebView webView, NSUrlAuthenticationChallenge challenge, Action<NSUrlSessionAuthChallengeDisposition, NSUrlCredential> completionHandler)
+        {
+            completionHandler(NSUrlSessionAuthChallengeDisposition.UseCredential, new NSUrlCredential(challenge.ProtectionSpace.ServerSecTrust));
+
+            return;
+        }
+    }
+}
+
